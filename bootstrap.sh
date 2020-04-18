0ecc0218da359e9c166f52268c57f0abcc1407dd
David Garvey <david@tyk.io>
2020-04-18 20:47:19 +0800

    Fixed kibana visualisation bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-DASHBOARD_BASE_URL="http://localhost:3000"
-GATEWAY_BASE_URL="http://localhost:8080"
-KIBANA_BASE_URL="http://localhost:5601"
+dashboard_base_url="http://localhost:3000"
+gateway_base_url="http://localhost:8080"
+kibana_base_url="http://localhost:5601"
 
 echo "Getting Dashboard Admin API credentials"
-DASHBOARD_ADMIN_API_CREDENTIALS=$(cat ./confs/tyk_analytics.conf | jq -r .admin_secret)
-echo "  Dashboard Admin API Credentials: $DASHBOARD_ADMIN_API_CREDENTIALS"
+dashboard_admin_api_credentials=$(cat ./confs/tyk_analytics.conf | jq -r .admin_secret)
+echo "  Dashboard Admin API Credentials: $dashboard_admin_api_credentials"
 
 echo "Creating Organisation"
-ORGANISATION_ID=$(curl $DASHBOARD_BASE_URL/admin/organisations \
+organisation_id=$(curl $dashboard_base_url/admin/organisations \
   --silent \
-  --header "admin-auth: $DASHBOARD_ADMIN_API_CREDENTIALS" \
+  --header "admin-auth: $dashboard_admin_api_credentials" \
   --data @bootstrap-data/tyk-dashboard/organisation.json \
   | jq -r '.Meta')
-echo "  Organisation Id: $ORGANISATION_ID"
+echo "  Organisation Id: $organisation_id"
 
 echo "Creating Dashboard user"
-DASHBOARD_USER_FIRST_NAME=$(jq -r '.first_name' bootstrap-data/tyk-dashboard/user.json)
-DASHBOARD_USER_LAST_NAME=$(jq -r '.last_name' bootstrap-data/tyk-dashboard/user.json)
-DASHBOARD_USER_EMAIL=$(jq -r '.email_address' bootstrap-data/tyk-dashboard/user.json)
-DASHBOARD_USER=$(curl $DASHBOARD_BASE_URL/admin/users \
+dashboard_user_first_name=$(jq -r '.first_name' bootstrap-data/tyk-dashboard/user.json)
+dashboard_user_last_name=$(jq -r '.last_name' bootstrap-data/tyk-dashboard/user.json)
+dashboard_user_email=$(jq -r '.email_address' bootstrap-data/tyk-dashboard/user.json)
+dashboard_user=$(curl $dashboard_base_url/admin/users \
   --silent \
-  --header "admin-auth: $DASHBOARD_ADMIN_API_CREDENTIALS" \
+  --header "admin-auth: $dashboard_admin_api_credentials" \
   --data-raw '{
-      "first_name": "'$DASHBOARD_USER_FIRST_NAME'",
-      "last_name": "'$DASHBOARD_USER_LAST_NAME'",
-      "email_address": "'$DASHBOARD_USER_EMAIL'",
-      "org_id": "'$ORGANISATION_ID'",
+      "first_name": "'$dashboard_user_first_name'",
+      "last_name": "'$dashboard_user_last_name'",
+      "email_address": "'$dashboard_user_email'",
+      "org_id": "'$organisation_id'",
       "active": true,
       "user_permissions": {
           "IsAdmin": "admin",
@@ -35,36 +35,36 @@ DASHBOARD_USER=$(curl $DASHBOARD_BASE_URL/admin/users \
       }
     }' \
     | jq -r '. | {api_key:.Message, id:.Meta.id}')
-DASHBOARD_USER_ID=$(echo $DASHBOARD_USER | jq -r '.id')
-DASHBOARD_USER_API_CREDENTIALS=$(echo $DASHBOARD_USER | jq -r '.api_key')
-DASHBOARD_USER_PASSWORD=$(openssl rand -base64 12)
-curl $DASHBOARD_BASE_URL/api/users/$DASHBOARD_USER_ID/actions/reset \
+dashboard_user_id=$(echo $dashboard_user | jq -r '.id')
+dashboard_user_api_credentials=$(echo $dashboard_user | jq -r '.api_key')
+dashboard_user_password=$(openssl rand -base64 12)
+curl $dashboard_base_url/api/users/$dashboard_user_id/actions/reset \
   --silent \
-  --header "authorization: $DASHBOARD_USER_API_CREDENTIALS" \
+  --header "authorization: $dashboard_user_api_credentials" \
   --data-raw '{
-      "new_password":"'$DASHBOARD_USER_PASSWORD'",
+      "new_password":"'$dashboard_user_password'",
       "user_permissions": { "IsAdmin": "admin" }
     }' \
   > /dev/null
-echo "  Username: $DASHBOARD_USER_EMAIL"
-echo "  Password: $DASHBOARD_USER_PASSWORD"
-echo "  Dashboard API Credentials: $DASHBOARD_USER_API_CREDENTIALS"
-echo "  ID: $DASHBOARD_USER_ID"
+echo "  Username: $dashboard_user_email"
+echo "  Password: $dashboard_user_password"
+echo "  Dashboard API Credentials: $dashboard_user_api_credentials"
+echo "  ID: $dashboard_user_id"
 
 echo "Creating Portal home page"
-curl $DASHBOARD_BASE_URL/api/portal/pages \
+curl $dashboard_base_url/api/portal/pages \
   --silent \
-  --header "Authorization: $DASHBOARD_USER_API_CREDENTIALS" \
+  --header "Authorization: $dashboard_user_api_credentials" \
   --data '{"is_homepage": true, "template_name":"", "title":"Developer Portal Home", "slug":"/", "fields": {"JumboCTATitle": "Tyk Developer Portal", "SubHeading": "Sub Header", "JumboCTALink": "#cta", "JumboCTALinkTitle": "Your awesome APIs, hosted with Tyk!", "PanelOneContent": "Panel 1 content.", "PanelOneLink": "#panel1", "PanelOneLinkTitle": "Panel 1 Button", "PanelOneTitle": "Panel 1 Title", "PanelThereeContent": "", "PanelThreeContent": "Panel 3 content.", "PanelThreeLink": "#panel3", "PanelThreeLinkTitle": "Panel 3 Button", "PanelThreeTitle": "Panel 3 Title", "PanelTwoContent": "Panel 2 content.", "PanelTwoLink": "#panel2", "PanelTwoLinkTitle": "Panel 2 Button", "PanelTwoTitle": "Panel 2 Title"}}' \
   > /dev/null
 echo "  Done"
 
 echo "Synchronising APIs and Policies"
-tyk-sync sync -d $DASHBOARD_BASE_URL -s $DASHBOARD_USER_API_CREDENTIALS -o $ORGANISATION_ID -p tyk-sync-data
+tyk-sync sync -d $dashboard_base_url -s $dashboard_user_api_credentials -o $organisation_id -p tyk-sync-data
 echo "  Done"
 
 echo "Making test API call"
-curl $GATEWAY_BASE_URL:8080/bootstrap-api/get \
+curl $gateway_base_url:8080/bootstrap-api/get \
   --silent \
   > /dev/null
 echo "  Done"
@@ -73,7 +73,7 @@ echo "Waiting for Kibana to be available (please be patient)"
 kibana_status=""
 while [[ $kibana_status != "200" ]]
 do
-  kibana_status=$(curl -I $KIBANA_BASE_URL/app/kibana 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+  kibana_status=$(curl -I $kibana_base_url/app/kibana 2>/dev/null | head -n 1 | cut -d$' ' -f2)
   if [[ $kibana_status != "200" ]]
   then
     echo "  Kibana not ready yet, waiting 5 seconds to try again"
@@ -83,22 +83,23 @@ done
 echo "  Done"
 
 echo "Setting up Kibana objects"
-curl $KIBANA_BASE_URL/api/saved_objects/index-pattern/1208b8f0-815b-11ea-b0b2-c9a8a88fbfb2?overwrite=true \
+curl $kibana_base_url/api/saved_objects/index-pattern/1208b8f0-815b-11ea-b0b2-c9a8a88fbfb2?overwrite=true \
   --silent \
   --header 'Content-Type: application/json' \
   --header 'kbn-xsrf: true' \
   --data @bootstrap-data/kibana/index-patterns/tyk-analytics.json \
   > /dev/null
-curl $KIBANA_BASE_URL:5601/api/saved_objects/visualization/407e91c0-8168-11ea-9323-293461ad91e5?overwrite=true \
+curl $kibana_base_url/api/saved_objects/visualization/407e91c0-8168-11ea-9323-293461ad91e5?overwrite=true \
   --silent \
   --header 'Content-Type: application/json' \
   --header 'kbn-xsrf: true' \
   --data @bootstrap-data/kibana/visualizations/requests-in-last-30-minutes.json \
   > /dev/null
+echo "  Done"
 
 echo "--------------------------------------"
 echo "Bootstrap complete"
 echo "Dashboard login information"
-echo "  Dashboard URL: $DASHBOARD_BASE_URL"
-echo "  Username: $DASHBOARD_USER_EMAIL"
-echo "  Password: $DASHBOARD_USER_PASSWORD"
+echo "  Dashboard URL: $dashboard_base_url"
+echo "  Username: $dashboard_user_email"
+echo "  Password: $dashboard_user_password"
