68e8ccc8ffd37b5a2b92558cb48a05d6a412f850
David Garvey <david@tyk.io>
2020-05-07 14:57:18 +0800

    bootstrap

--- cicd/bootstrap.sh
+++ cicd/bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+dot_count=""
+
+function bootstrap_progress {
+  dot_count=$((dot_count+1))
+  dots=$(printf "%-${dot_count}s" ".")
+  echo -ne "  Bootstrapping Jenkins ${dots// /.} \r"
+}
+
 jenkins_base_url="http://localhost:8070"
 jenkins_status=""
 # 403 indicates that at least Jenkins was able to recognise that the request was unauthorised, so we should be ok to proceed
@@ -8,40 +16,37 @@ jenkins_tries=0
 
 while [ "$jenkins_status" != "$jenkins_status_desired" ]
 do
-  jenkins_tries=$((jenkins_tries+1))
-  dot=$(printf "%-${jenkins_tries}s" ".")
-  echo -ne "  Bootstrapping Jenkins ${dot// /.} \r"
   jenkins_status=$(curl -I -m2 $jenkins_base_url 2>/dev/null | head -n 1 | cut -d$' ' -f2)
-
   if [ "$jenkins_status" != "$jenkins_status_desired" ]
   then
     sleep 1
   fi
+  bootstrap_progress
 done
 
 jenkins_admin_password=$(docker-compose -f docker-compose.yml -f cicd/docker-compose.yml exec jenkins sh -c "cat /var/jenkins_home/secrets/initialAdminPassword | head -c32")
+bootstrap_progress
 
 # extract plugins and other configuration
 docker-compose -f docker-compose.yml -f cicd/docker-compose.yml exec \
   jenkins \
   tar -xzvf /var/jenkins_home/jenkins.tar.gz -C /var/jenkins_home > /dev/null
+bootstrap_progress
 
 # restart contaner to allow new config and plugins to be used
 docker-compose -f docker-compose.yml -f cicd/docker-compose.yml restart jenkins 2> /dev/null
+bootstrap_progress
 
 # create job for 'APIs and Policies'
 jenkins_response=""
 while [ "${jenkins_response:0:1}" != "0" ]
 do
-  jenkins_tries=$((jenkins_tries+1))
-  dot=$(printf "%-${jenkins_tries}s" ".")
-  echo -ne "  Bootstrapping Jenkins ${dot// /.} \r"
   jenkins_response=$(docker-compose -f docker-compose.yml -f cicd/docker-compose.yml exec jenkins bash -c "java -jar /var/jenkins_home/jenkins-cli.jar -s http://localhost:8080/ -auth admin:$jenkins_admin_password -webSocket create-job 'apis-and-policies'</var/jenkins_home/job-apis-and-policies.xml; echo $?")
-
   if [ "${jenkins_response:0:1}" != "0" ]
   then
     sleep 1
   fi
+  bootstrap_progress
 done
 
 echo -e "\033[2K           Jenkins
--- tyk-2/bootstrap.sh
+++ tyk-2/bootstrap.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
+dot_count=""
+
+function bootstrap_progress {
+  dot_count=$((dot_count+1))
+  dots=$(printf "%-${dot_count}s" ".")
+  echo -ne "  Bootstrapping Tyk Environment 2 ${dots// /.} \r"
+}
+
 dashboard2_base_url="http://localhost:3002"
 gateway2_base_url="http://localhost:8085"
 dashboard_admin_api_credentials=$(cat tyk/volumes/tyk-dashboard/tyk_analytics.conf | jq -r .admin_secret)
 
 status=""
 status_desired="200"
-tries=0
 
 while [ "$status" != "$status_desired" ]
 do
-  tries=$((tries+1))
-  dot=$(printf "%-${tries}s" ".")
-  echo -ne "  Bootstrapping Tyk Environment 2 ${dot// /.} \r"
-  status=$(curl -I -m2 $dashboard2_base_url/admin/organisations -H "admin-auth: $dashboard_admin_api_credentials" 2>/dev/null | head -n 1 | cut -d$' ' -f2)
-  
+  status=$(curl -I -m2 $dashboard2_base_url/admin/organisations -H "admin-auth: $dashboard_admin_api_credentials" 2>/dev/null | head -n 1 | cut -d$' ' -f2)  
   if [ "$status" != "$status_desired" ]
   then
     sleep 1
   fi
+  bootstrap_progress
 done
 
 curl $dashboard2_base_url/admin/organisations/import -s \
   -H "admin-auth: $dashboard_admin_api_credentials" \
   -d @tyk/bootstrap/tyk-dashboard/organisation.json > /dev/null
+bootstrap_progress
 
 dashboard_user_email=$(jq -r '.email_address' tyk/bootstrap/tyk-dashboard/dashboard-user.json)
 dashboard_user_password=$(jq -r '.password' tyk/bootstrap/tyk-dashboard/dashboard-user.json)
@@ -33,6 +38,8 @@ dashboard2_user_api_response=$(curl $dashboard2_base_url/admin/users -s \
   | jq -r '. | {api_key:.Message, id:.Meta.id}')
 dashboard2_user_id=$(echo $dashboard2_user_api_response | jq -r '.id')
 dashboard2_user_api_credentials=$(echo $dashboard2_user_api_response | jq -r '.api_key')
+bootstrap_progress
+
 curl $dashboard2_base_url/api/users/$dashboard2_user_id/actions/reset -s \
   -H "authorization: $dashboard2_user_api_credentials" \
   --data-raw '{
