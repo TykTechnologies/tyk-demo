abf0115d23942bc914c5f771b6a08f5718caaf5c
David Garvey <david@tyk.io>
2020-05-10 20:32:35 +0800

    logging messages

--- tracing/bootstrap.sh
+++ tracing/bootstrap.sh
@@ -12,39 +12,42 @@ zipkin_base_url="http://localhost:9411/zipkin/"
 zipkin_status=""
 zipkin_status_desired="200"
 
-echo "Wait for zipkin to respond ok" >>bootstrap.log
+echo "Waiting for zipkin to respond ok" >>bootstrap.log
 while [ "$zipkin_status" != "$zipkin_status_desired" ]
 do
   zipkin_status=$(curl -I -s -m5 $zipkin_base_url 2>>bootstrap.log | head -n 1 | cut -d$' ' -f2)
   if [ "$zipkin_status" != "$zipkin_status_desired" ]
   then
-    echo "Zipkin status:$zipkin_status" >>bootstrap.log
+    echo "  Zipkin status:$zipkin_status" >>bootstrap.log
     sleep 1
+  else
+    echo "  Ok"
   fi
   bootstrap_progress
 done
 
-echo "Check tracing env var is set correctly" >>bootstrap.log
+echo "Checking tracing env var is set correctly" >>bootstrap.log
 tracing_setting=$(grep "TRACING_ENABLED" .env)
 tracing_setting_desired="TRACING_ENABLED=true"
-
 if [[ $tracing_setting != $tracing_setting_desired ]]
 then
-     # if missing
-     if [ ${#tracing_setting} == 0 ]
-     then
-          echo "  Adding tracing docker environment variable to .env file" >>bootstrap.log
-          echo $tracing_setting_desired >> .env
-     else
-          echo "  Setting tracing docker envionment variable to desired value" >>bootstrap.log
-          sed -i.bak 's/'"$tracing_setting"'/'"$tracing_setting_desired"'/g' ./.env
-          rm .env.bak
-     fi
-     bootstrap_progress
-     
-     echo "  Restarting Tyk containers to take effect" >>bootstrap.log
-     docker-compose restart 2>/dev/null
-     bootstrap_progress
+  # if missing
+  if [ ${#tracing_setting} == 0 ]
+  then
+      echo "  Adding tracing docker environment variable to .env file" >>bootstrap.log
+      echo $tracing_setting_desired >> .env
+  else
+      echo "  Setting tracing docker envionment variable to desired value" >>bootstrap.log
+      sed -i.bak 's/'"$tracing_setting"'/'"$tracing_setting_desired"'/g' ./.env
+      rm .env.bak
+  fi
+  bootstrap_progress
+
+  echo "  Restarting Tyk containers to take effect" >>bootstrap.log
+  docker-compose restart 2>/dev/null
+  bootstrap_progress
+else
+  echo "  Ok"
 fi
 
 echo "End tracing bootstrap" >>bootstrap.log
