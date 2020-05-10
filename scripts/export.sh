ab1fc902ee1749b255cf7fcd738177f326a09e21
David Garvey <david@tyk.io>
2020-05-10 12:32:30 +0800

    Changed script back to basic approach

--- scripts/export.sh
+++ scripts/export.sh
@@ -4,29 +4,21 @@ dashboard_base_url="http://localhost:3000"
 dashboard_user_api_credentials=$(cat .context-data/dashboard-user-api-credentials)
 
 echo "Exporting APIs"
-api_data=$(cat tyk/data/tyk-dashboard/import-template-apis-base.json)
-api_ids_to_export=$(curl $dashboard_base_url/api/apis?p=-1 -s \
-  -H "Authorization:$dashboard_user_api_credentials" | \
-  jq --raw-output '.apis[].api_definition.api_id')
-for api_id in $api_ids_to_export
+curl $dashboard_base_url/api/apis?p=-1 -s \
+  -H "Authorization:$dashboard_user_api_credentials" \
+  | jq '.' \
+  > tyk/data/tyk-dashboard/apis.json
+cat tyk/data/tyk-dashboard/apis.json | jq --raw-output '.apis[].api_definition.name' | while read api_name
 do
-  api_definition=$(curl $dashboard_base_url/api/apis/$api_id -s \
-    -H "Authorization:$dashboard_user_api_credentials")
-  echo "  $(echo "$api_definition" | jq -r .api_definition.name)"
-  api_data=$(echo $api_data | jq --argjson api_definition "$api_definition" '.apis += [$api_definition]')
+  echo "  $api_name"
 done
-echo $api_data | jq '.' > tyk/data/tyk-dashboard/apis.json
 
 echo "Exporting Policies"
-policy_data=$(cat tyk/data/tyk-dashboard/import-template-policies-base.json)
-policy_ids_to_export=$(curl $dashboard_base_url/api/portal/policies?p=-1 -s \
-  -H "Authorization:$dashboard_user_api_credentials" | \
-  jq --raw-output '.Data[]._id')
-for policy_id in $policy_ids_to_export
+curl $dashboard_base_url/api/portal/policies?p=-1 -s \
+  -H "Authorization:$dashboard_user_api_credentials" \
+  | jq '.' \
+  > tyk/data/tyk-dashboard/policies.json
+cat tyk/data/tyk-dashboard/policies.json | jq --raw-output '.Data[].name' | while read policy_name
 do
-  policy_definition=$(curl $dashboard_base_url/api/portal/policies/$policy_id -s \
-    -H "Authorization:$dashboard_user_api_credentials")
-  echo "  $(echo "$policy_definition" | jq -r .name)"
-  policy_data=$(echo $policy_data | jq --argjson policy_definition "$policy_definition" '.Data += [$policy_definition]')
+  echo "  $policy_name"
 done
-echo $policy_data | jq '.' > tyk/data/tyk-dashboard/policies.json
