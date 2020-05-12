6e87c345ceec0c51e43bce9ccfd7d40cbef535a9
David Garvey <david@tyk.io>
2020-05-12 22:48:59 +0800

    fixes for docker-compose command

--- instrumentation/bootstrap.sh
+++ instrumentation/bootstrap.sh
@@ -16,17 +16,16 @@ then
      log_message "  Adding instrumentation setting to docker env var file"
      echo $instrumentation_setting_desired >> .env
   else
-     log_message "  Updating instrumentation docker env var"
+     log_message "  Setting instrumentation docker env var to 1"
      sed -i.bak 's/'"$instrumentation_setting"'/'"$instrumentation_setting_desired"'/g' ./.env
      rm .env.bak
   fi
   bootstrap_progress
-  log_message "  Restarting tyk containers to take effect"
-  docker-compose restart 2> /dev/null
+  log_message "  Recreating Tyk containers to take effect"
+  docker-compose -f tyk/docker-compose.yml -p tyk-pro-docker-demo-extended up --force-recreate --no-deps -d tyk-dashboard tyk-gateway tyk-pump
   bootstrap_progress
-else
-  log_ok
 fi
+log_ok
 
 log_end_feature
 
