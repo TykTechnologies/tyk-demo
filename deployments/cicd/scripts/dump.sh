5df9c64c439f3a843b0c30711142fbfe22bc3579
David Garvey <david@tyk.io>
2020-05-25 12:19:18 +0800

    updated mapped volume file

--- deployments/cicd/scripts/dump.sh
+++ deployments/cicd/scripts/dump.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+
 docker-compose \
     -f deployments/tyk/docker-compose.yml \
     -f deployments/cicd/docker-compose.yml \
     -p tyk-pro-docker-demo-extended \
     --project-directory $(pwd) \
-    exec -u git gitea sh -c "cd /data/dump; gitea dump -c /data/gitea/conf/app.ini"
+    exec -u git gitea sh -c "cd /data/gitea; rm gitea-dump.zip; gitea dump -c /data/gitea/conf/app.ini --f gitea-dump.zip"
 
+gitea_container_id=$(docker-compose \
+    -f deployments/tyk/docker-compose.yml \
+    -f deployments/cicd/docker-compose.yml \
+    -p tyk-pro-docker-demo-extended \
+    --project-directory $(pwd) \
+    ps -q gitea)
 
-# use this command for zip?
-# zip -r file.zip . -x "gitea/indexers/*" "gitea/queues/*"
+docker cp $gitea_container_id:/data/gitea/gitea-dump.zip deployments/cicd/volumes/gitea/gitea-dump.zip
