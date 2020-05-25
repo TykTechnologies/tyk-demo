1ab1195b094fbb6fe6822553118731d8909854d0
David Garvey <david@tyk.io>
2020-05-25 13:46:36 +0800

    file rename

---
+++ deployments/cicd/scripts/dump-tyk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dashboard_user_api_credentials=$(cat .context-data/dashboard-user-api-credentials)
+docker run --rm \
+  --network tyk-pro-docker-demo-extended_tyk \
+  -v $(pwd)/deployments/cicd/gitea/tyk-data:/opt/tyk-sync/data \
+  tykio/tyk-sync:v1.1.0 \
+  dump -d http://tyk-dashboard:3000 -s $dashboard_user_api_credentials -t data
--- deployments/cicd/scripts/restore.sh
+++
@@ -1,6 +0,0 @@
-docker-compose \
-    -f deployments/tyk/docker-compose.yml \
-    -f deployments/cicd/docker-compose.yml \
-    -p tyk-pro-docker-demo-extended \
-    --project-directory $(pwd) \
-    exec gitea sh -c "chmod +x /data/restore.sh; ./data/restore.sh"
