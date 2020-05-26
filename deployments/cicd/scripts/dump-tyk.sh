90048ce7ad77634cccda689e42ae1e90833d64ed
David Garvey <david@tyk.io>
2020-05-26 23:49:44 +0800

    cicd script updates

--- deployments/cicd/scripts/dump-tyk.sh
+++ deployments/cicd/scripts/dump-tyk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # uses Tyk Sync to extract API and Policy data Gitea tyk-data repository
+
 dashboard_user_api_credentials=$(cat .context-data/dashboard-user-api-credentials)
 gitea_tyk_data_repo_path=$(cat .context-data/gitea-tyk-data-repo-path)
 docker run --rm \
