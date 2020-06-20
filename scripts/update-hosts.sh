b03574923c53715d29d3361345d6bd841ca64801
David Garvey <davegarvey@hotmail.com>
2020-06-21 00:05:27 +0800

    changed to use tyk-custom-domain

--- scripts/update-hosts.sh
+++ scripts/update-hosts.sh
@@ -14,5 +14,5 @@ echo "127.0.0.1	tyk-dashboard.localhost" >> /etc/hosts
 echo "127.0.0.1	tyk-portal.localhost" >> /etc/hosts
 echo "127.0.0.1	tyk-gateway.localhost" >> /etc/hosts
 echo "127.0.0.1	tyk-gateway-2.localhost" >> /etc/hosts
-echo "127.0.0.1	custom-domain.localhost" >> /etc/hosts
+echo "127.0.0.1	tyk-custom-domain.localhost" >> /etc/hosts
 echo "# End of section" >> /etc/hosts
