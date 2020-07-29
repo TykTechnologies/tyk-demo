03ba1caed7e3299822c10b613665c303fce8709c
David Garvey <davegarvey@hotmail.com>
2020-07-29 15:41:34 +0800

    Improved hosts management

--- scripts/update-hosts.sh
+++ scripts/update-hosts.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-section_phase="# Added by Tyk Demo"
+source scripts/common.sh
 
-if grep -q "$section_phase" /etc/hosts; then
-    echo "Tyk host entries already exist"
-    exit
+section_phrase="# Added by Tyk Demo"
+
+if ! grep -q "$section_phrase" /etc/hosts; then
+  echo "Adding Tyk Demo section to /etc/hosts"
+  echo "$section_phrase" >> /etc/hosts
+  echo "# End of section" >> /etc/hosts
 fi
 
-echo "Adding Tyk host entries"
-
-echo "$section_phase" >> /etc/hosts
-echo "127.0.0.1	tyk-dashboard.localhost" >> /etc/hosts
-echo "127.0.0.1	tyk-portal.localhost" >> /etc/hosts
-echo "127.0.0.1	tyk-gateway.localhost" >> /etc/hosts
-echo "127.0.0.1	tyk-gateway-2.localhost" >> /etc/hosts
-echo "127.0.0.1	tyk-custom-domain.localhost" >> /etc/hosts
-echo "127.0.0.1	tyk-worker-gateway.localhost" >> /etc/hosts
-echo "# End of section" >> /etc/hosts
+for i in "${tyk_demo_hostnames[@]}"
+do
+  desired_host_config="127.0.0.1	$i"
+  if ! grep -q "$desired_host_config" /etc/hosts; then
+    echo "Adding $i"
+    sed -i.bak 's/'"$section_phrase"'/'"$section_phrase"'\
+'"$desired_host_config"'/' /etc/hosts
+    rm /etc/hosts.bak
+  fi
+done
