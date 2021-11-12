4cab93939f133f9dce6c1a5f177277b96cdf272c
David Garvey <davegarvey@hotmail.com>
2021-11-12 18:16:56 +0800

    docker compose v1/2 compatibility

--- deployments/cicd/scripts/dump-gitea.sh
+++ deployments/cicd/scripts/dump-gitea.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
+source scripts/common.sh
+
 # generates a Gitea archive dump of the configuration and repositories
 # then copies the file from the container and overwrites the tracked archive in this repo
 
-docker-compose \
-    -f deployments/tyk/docker-compose.yml \
-    -f deployments/cicd/docker-compose.yml \
-    -p tyk-demo \
-    --project-directory $(pwd) \
-    exec -u git gitea sh -c "cd /data/gitea; rm gitea-dump.zip; gitea dump -c /data/gitea/conf/app.ini --f gitea-dump.zip"
+check_docker_compose_version
 
-gitea_container_id=$(docker-compose \
-    -f deployments/tyk/docker-compose.yml \
-    -f deployments/cicd/docker-compose.yml \
-    -p tyk-demo \
-    --project-directory $(pwd) \
-    ps -q gitea)
+command_docker_compose="$(generate_docker_compose_command) exec -T -u git gitea sh -c \"cd /data/gitea; rm gitea-dump.zip; gitea dump -c /data/gitea/conf/app.ini --f gitea-dump.zip\""
+eval $command_docker_compose
+
+command_docker_compose="$(generate_docker_compose_command) ps -q gitea"
+gitea_container_id=$(eval $command_docker_compose)
 
 docker cp $gitea_container_id:/data/gitea/gitea-dump.zip deployments/cicd/volumes/gitea/gitea-dump.zip
