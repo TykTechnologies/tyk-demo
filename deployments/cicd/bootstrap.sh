91f6a59bf7c8e177b271530ea0b158faa2a65237
David Garvey <davegarvey@hotmail.com>
2021-11-12 17:16:27 +0800

    fix jenkins CLI issue

--- deployments/cicd/bootstrap.sh
+++ deployments/cicd/bootstrap.sh
@@ -36,7 +36,7 @@ docker-compose \
     -f deployments/cicd/docker-compose.yml \
     -p tyk-demo \
     --project-directory $(pwd) \
-    exec gitea sh -c "./data/restore.sh" 1>> /dev/null 2>> bootstrap.log
+    exec -T gitea sh -c "./data/restore.sh" 1>> /dev/null 2>> bootstrap.log
 log_ok
 bootstrap_progress
 
@@ -46,7 +46,7 @@ docker-compose \
     -f deployments/cicd/docker-compose.yml \
     -p tyk-demo \
     --project-directory $(pwd) \
-    exec -u git gitea sh -c "gitea admin regenerate hooks;" 1>> /dev/null 2>> bootstrap.log
+    exec -T -u git gitea sh -c "gitea admin regenerate hooks;" 1>> /dev/null 2>> bootstrap.log
 log_ok
 bootstrap_progress
 
@@ -103,12 +103,12 @@ log_message "Waiting for Jenkins to respond ok"
 wait_for_response "$jenkins_base_url" "403"
 
 log_message "Getting Jenkins admin password"
-jenkins_admin_password=$(docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec jenkins sh -c "cat /var/jenkins_home/secrets/initialAdminPassword | head -c32" 2>> bootstrap.log)
+jenkins_admin_password=$(docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec -T jenkins sh -c "cat /var/jenkins_home/secrets/initialAdminPassword | head -c32" 2>> bootstrap.log)
 log_message "  Jenkins admin password = $jenkins_admin_password"
 bootstrap_progress
 
 log_message "Extracting Jenkins plugins and other configuration"
-docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec \
+docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec -T \
   jenkins \
   tar -xzvf /var/jenkins_home/bootstrap-import/jenkins-plugins.tar.gz -C /var/jenkins_home 1> /dev/null 2>> bootstrap.log
 log_ok
@@ -146,15 +146,30 @@ sed "s/TYK2_DASHBOARD_CREDENTIALS/$dashboard2_user_api_credentials/g" deployment
   deployments/cicd/volumes/jenkins/bootstrap-import/credentials-global.xml
 log_ok
 
+log_message "Waiting for Jenkins CLI to be ready"
+# After the container restart, Jenkins functionality will not work for a little while, so we have to test if it's ready by making some CLI calls
+jenkins_response=""
+while [ "$jenkins_response" == "" ]
+do
+  jenkins_response=$(docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory /Users/davidgarvey/git/tyk-demo exec -T jenkins bash -c "java -jar /var/jenkins_home/jenkins-cli.jar -s http://localhost:8080/ -auth admin:$jenkins_admin_password -webSocket who-am-i" 2>> /dev/null)
+  if [ "$jenkins_response" == "" ]
+  then
+    log_message "  Request unsuccessful, retrying..."
+    # Jenkins CLI calls are generally pretty slow, so there's no need to sleep here
+  else
+    log_ok
+  fi
+  bootstrap_progress
+done
+
 log_message "Importing credentials for 'global'"
 jenkins_response=""
 while [ "${jenkins_response:0:1}" != "0" ]
 do
-  jenkins_response=$(docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec jenkins bash -c "java -jar /var/jenkins_home/jenkins-cli.jar -s http://localhost:8080/ -auth admin:$jenkins_admin_password -webSocket import-credentials-as-xml system::system::jenkins < /var/jenkins_home/bootstrap-import/credentials-global.xml; echo $?")
+  jenkins_response=$(docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec -T jenkins bash -c "java -jar /var/jenkins_home/jenkins-cli.jar -s http://localhost:8080/ -auth admin:$jenkins_admin_password -webSocket import-credentials-as-xml system::system::jenkins < /var/jenkins_home/bootstrap-import/credentials-global.xml; echo $?")
   if [ "${jenkins_response:0:1}" != "0" ]
   then
     log_message "  Request unsuccessful, retrying..."
-    sleep 2
   else
     log_ok
   fi
@@ -165,11 +180,10 @@ log_message "Creating job for 'APIs and Policies'"
 jenkins_response=""
 while [ "${jenkins_response:0:1}" != "0" ]
 do
-  jenkins_response=$(docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec jenkins bash -c "java -jar /var/jenkins_home/jenkins-cli.jar -s http://localhost:8080/ -auth admin:$jenkins_admin_password -webSocket create-job 'apis-and-policies' < /var/jenkins_home/bootstrap-import/job-apis-and-policies.xml; echo $?")
+  jenkins_response=$(docker-compose -f deployments/tyk/docker-compose.yml -f deployments/cicd/docker-compose.yml -p tyk-demo --project-directory $(pwd) exec -T jenkins bash -c "java -jar /var/jenkins_home/jenkins-cli.jar -s http://localhost:8080/ -auth admin:$jenkins_admin_password -webSocket create-job 'apis-and-policies' < /var/jenkins_home/bootstrap-import/job-apis-and-policies.xml; echo $?")
   if [ "${jenkins_response:0:1}" != "0" ]
   then
     log_message "  Request unsuccessful, retrying..."
-    sleep 2
   else
     log_ok
   fi
