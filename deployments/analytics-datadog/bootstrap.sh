161effccfe06514098d9eeeab6ef95d9f55479e9
David Garvey <davegarvey@hotmail.com>
2021-11-17 10:43:03 +0800

    Improve log info

--- deployments/analytics-datadog/bootstrap.sh
+++ deployments/analytics-datadog/bootstrap.sh
@@ -6,26 +6,24 @@ log_start_deployment
 bootstrap_progress
 
 datadog_dashboard="https://app.datadoghq.com/dashboard/lists"
+pump_container_name="$(get_context_data "container" "pump" "1" "name")"
 
-
-log_message "Stopping the Pump instance deployed by the base deployment"
-# the tyk-pump container (from the tyk deployment) is stopped as it is replaced by the container from this deployment
+log_message "Stopping the Pump container ($pump_container_name) deployed by the Tyk deployment. This prevents the Tyk deployment Pump from consuming the analytics records which we want to be processed by the Pump from this deployment."
 command_docker_compose="$(generate_docker_compose_command) stop tyk-pump 2> /dev/null" 
 eval $command_docker_compose
 if [ "$?" != 0 ]; then
-  echo "Error stopping Pump container"
+  echo "Error stopping Pump container $pump_container_name"
   exit 1
 fi
 log_ok
 bootstrap_progress
 
-# verify tyk-pump container is stopped
-log_message "Checking status of Pump container"
-container_status=$(docker ps -a --filter "name=$(get_context_data "container" "pump" "1" "name")" --format "{{.Status}}")
-log_message "  Pump container status is: $container_status"
+log_message "Verifying that $pump_container_name container is stopped"
+container_status=$(docker ps -a --filter "name=$pump_container_name" --format "{{.Status}}")
+log_message "  $pump_container_name container status is: $container_status"
 if [[ $container_status != Exited* ]]
 then
-  log_message "  ERROR: tyk-pump container is not stopped. Exiting."
+  log_message "  ERROR: $pump_container_name container is not stopped. Exiting."
   exit 1
 fi
 log_ok
