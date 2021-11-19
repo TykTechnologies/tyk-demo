7326ec5a0488feb14f283d012c9c8f8333dcb6b0
David Garvey <davegarvey@hotmail.com>
2021-11-19 14:43:46 +0800

    include all command arguments

--- docker-compose-command.sh
+++ docker-compose-command.sh
@@ -5,7 +5,7 @@ source scripts/common.sh
 # check if docker compose version is v1.x
 check_docker_compose_version
 
-docker_compose_command="$(generate_docker_compose_command) $1"
+docker_compose_command="$(generate_docker_compose_command) $@"
 
 echo "Running command: $docker_compose_command"
 
