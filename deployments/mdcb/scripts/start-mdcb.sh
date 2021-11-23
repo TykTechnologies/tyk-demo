661dc2e1e0dd19bc01016603f0e064a1eb5f6539
David Garvey <davegarvey@hotmail.com>
2021-11-23 15:28:12 +0800

    simplify

--- deployments/mdcb/scripts/start-mdcb.sh
+++ deployments/mdcb/scripts/start-mdcb.sh
@@ -4,5 +4,4 @@ source scripts/common.sh
 
 check_docker_compose_version
 
-command_docker_compose="$(generate_docker_compose_command) start tyk-mdcb"
-eval $command_docker_compose
+eval $(generate_docker_compose_command) start tyk-mdcb
