3ab2f4bc182785523ddd7d272f88cf6eaecdbe63
Sedky <sedkyaboushamalah@gmail.com>
2021-12-13 09:12:31 -0500

    rebuild plugins

--- deployments/tyk/bootstrap.sh
+++ deployments/tyk/bootstrap.sh
@@ -135,11 +135,11 @@ bootstrap_progress
 # Go plugins
 
 # NOTE: Go plugins are commented out until go compiler issue is resolved
-# build_go_plugin "example-go-plugin.so" "example"
-# bootstrap_progress
+build_go_plugin "example-go-plugin.so" "example"
+bootstrap_progress
 
-# build_go_plugin "jwt-go-plugin.so" "jwt"
-# bootstrap_progress
+build_go_plugin "jwt-go-plugin.so" "jwt"
+bootstrap_progress
 
 # Dashboard Data
 
