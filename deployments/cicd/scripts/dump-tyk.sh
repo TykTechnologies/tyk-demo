7263ed01c90e90c4281abe942ca55ee4b338a99f
David Garvey <davegarvey@hotmail.com>
2022-01-14 14:19:58 +0800

    update comment

--- deployments/cicd/scripts/dump-tyk.sh
+++ deployments/cicd/scripts/dump-tyk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# uses Tyk Sync to extract API and Policy data Gitea tyk-data repository
+# uses Tyk Sync to extract API and Policy data from Tyk, and store it in the Gitea tyk-data repository directory
 
 dashboard_user_api_credentials=$(cat .context-data/1-dashboard-user-1-api-key)
 gitea_tyk_data_repo_path=$(cat .context-data/gitea-tyk-data-repo-path)
