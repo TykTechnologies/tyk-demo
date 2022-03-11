bd71037b18375fc8c47da91a8a765571096fb6ae
Sedky <sedkyaboushamalah@gmail.com>
2022-03-11 12:13:02 -0500

    enable playground on supergraph

--- deployments/federation/bootstrap.sh
+++ deployments/federation/bootstrap.sh
@@ -11,13 +11,13 @@ dashboard_admin_api_credentials=$(cat deployments/tyk/volumes/tyk-dashboard/tyk_
 dashboard_user_api_key=$(get_context_data "1" "dashboard-user" "1" "api-key")
 
 # Create APIs for Subgraphs and Federation
-create_api "deployments/federation/data/apis-1.json" "$dashboard_admin_api_credentials" "$dashboard_user_api_key"
+create_api "deployments/federation/data/apis-users.json" "$dashboard_admin_api_credentials" "$dashboard_user_api_key"
 bootstrap_progress
 
-create_api "deployments/federation/data/apis-2.json" "$dashboard_admin_api_credentials" "$dashboard_user_api_key"
+create_api "deployments/federation/data/apis-posts.json" "$dashboard_admin_api_credentials" "$dashboard_user_api_key"
 bootstrap_progress
 
-create_api "deployments/federation/data/apis-3.json" "$dashboard_admin_api_credentials" "$dashboard_user_api_key"
+create_api "deployments/federation/data/apis-supergraph.json" "$dashboard_admin_api_credentials" "$dashboard_user_api_key"
 bootstrap_progress
 
 log_end_deployment
