5b5303d420723f492c79e146028930bd6a0b9e38
David Garvey <davegarvey@hotmail.com>
2022-04-01 17:35:48 +0800

    Tidy

--- deployments/portal/bootstrap.sh
+++ deployments/portal/bootstrap.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 source scripts/common.sh
-deployment="portal"
+deployment="Portal"
 
 log_start_deployment
 bootstrap_progress
-
 
 # Grab the Dashboard License line from ENV file
 licence_line=$(grep "DASHBOARD_LICENCE=" .env)
@@ -16,7 +15,6 @@ encoded_licence_jwt=$(echo $licence_line | sed -E 's/^[A-Z_]+=(.+)$/\1/')
 dashboard_user_api_credentials=$(cat .context-data/1-dashboard-user-1-api-key)
 
 log_message "Exporting Docker Environment Variables for Enterprise Portal in .env"
-
 # Export to envs for docker
 set_docker_environment_value "ADMIN_EMAIL" $(cat .context-data/1-dashboard-user-1-email)
 set_docker_environment_value "ADMIN_PASSWORD" $(cat .context-data/1-dashboard-user-1-password)
@@ -29,14 +27,14 @@ set_docker_environment_value "PORTAL_LICENSEKEY" $encoded_licence_jwt
 set_docker_environment_value "TYK_DASHBOARD_API_ACCESS_CREDENTIALS" $dashboard_user_api_credentials
 
 
-log_message "Restarting tyk-portal ... "
+log_message "Recreating tyk-portal for new env vars"
 $(generate_docker_compose_command) rm -f -s tyk-portal 1>/dev/null 2>&1
 $(generate_docker_compose_command) up -d tyk-portal 2>/dev/null
-
+bootstrap_progress
 log_ok
 
 # Create Plans and Policies for NEW Developer Portal
-log_message "Creating Enterprise Portal Plans ..."
+log_message "Creating Enterprise Portal Plans"
 for file in deployments/portal/volumes/tyk-portal/plans/*; do 
   if [[ -f $file ]]; then
   	policy=`cat $file`
@@ -45,7 +43,9 @@ for file in deployments/portal/volumes/tyk-portal/plans/*; do
       -H "authorization: $dashboard_user_api_credentials" \
       -d "$policy"
   fi
+  bootstrap_progress
 done
+log_ok
 
 log_message "Creating Enterprise Portal Products ..."
 for file in deployments/portal/volumes/tyk-portal/products/*; do 
@@ -56,16 +56,18 @@ for file in deployments/portal/volumes/tyk-portal/products/*; do
       -H "authorization: $dashboard_user_api_credentials" \
       -d "$policy"
   fi
+  bootstrap_progress
 done
-
 log_ok
+
+log_end_deployment
 
 portal_admin_user_email=$(cat .context-data/1-dashboard-user-1-email)
 portal_admin_user_password=$(cat .context-data/1-dashboard-user-1-password)
 
 echo -e "\033[2K 
+▼ Portal
   ▽ Enterprise Portal ($(get_service_image_tag "tyk-portal"))
                     URL : http://tyk-portal.localhost:3100/
          Admin Username : $portal_admin_user_email
-         Admin Password : $portal_admin_user_password
-"
+         Admin Password : $portal_admin_user_password"
