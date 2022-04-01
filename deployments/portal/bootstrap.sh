b5ef3dcfafb0c9605ad2fa1daee5f4981436c455
David Garvey <davegarvey@hotmail.com>
2022-04-01 17:46:30 +0800

    tidy

--- deployments/portal/bootstrap.sh
+++ deployments/portal/bootstrap.sh
@@ -14,8 +14,8 @@ encoded_licence_jwt=$(echo $licence_line | sed -E 's/^[A-Z_]+=(.+)$/\1/')
 # Get Tyk Dashboard API Access Credentials
 dashboard_user_api_credentials=$(cat .context-data/1-dashboard-user-1-api-key)
 
-log_message "Exporting Docker Environment Variables for Enterprise Portal in .env"
 # Export to envs for docker
+log_message "Exporting Docker Environment Variables for Enterprise Portal in .env"
 set_docker_environment_value "ADMIN_EMAIL" $(cat .context-data/1-dashboard-user-1-email)
 set_docker_environment_value "ADMIN_PASSWORD" $(cat .context-data/1-dashboard-user-1-password)
 set_docker_environment_value "ADMIN_ORG_ID" $(cat .context-data/1-organisation-1-id)
@@ -25,7 +25,6 @@ set_docker_environment_value "PORTAL_THEMING_THEME" default
 set_docker_environment_value "PORTAL_THEMING_PATH" ./themes
 set_docker_environment_value "PORTAL_LICENSEKEY" $encoded_licence_jwt
 set_docker_environment_value "TYK_DASHBOARD_API_ACCESS_CREDENTIALS" $dashboard_user_api_credentials
-
 
 log_message "Recreating tyk-portal for new env vars"
 $(generate_docker_compose_command) rm -f -s tyk-portal 1>/dev/null 2>&1
@@ -50,11 +49,11 @@ log_ok
 log_message "Creating Enterprise Portal Products ..."
 for file in deployments/portal/volumes/tyk-portal/products/*; do 
   if [[ -f $file ]]; then
-  	policy=`cat $file`
+  	product=`cat $file`
     curl http://tyk-dashboard.localhost:3000/api/portal/policies -s \
       -o /dev/null \
       -H "authorization: $dashboard_user_api_credentials" \
-      -d "$policy"
+      -d "$product"
   fi
   bootstrap_progress
 done
