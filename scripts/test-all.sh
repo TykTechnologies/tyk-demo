97867c2c94285c625a0f2d262aa29c6143dbe7e9
David Garvey <davegarvey@hotmail.com>
2023-01-23 18:27:15 +0100

    progress

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# This script will run tests across all deployments, if they are provided, rather than just for the tyk deployment.
-# This requires that each deployment is sequentially created, tested and then removed. 
-# It may take a while to complete.
+# This script will run tests across all deployments, if they are provided, rather than just for the tyk deployment
+# This requires that each deployment is sequentially created, tested and then removed
+# It may take a while to complete
 # Note: script must be run from the repoistory root i.e. ./scripts/test-all.sh
-
-
-# check if there is a active deployment, and exit if so
 
 echo "Checking for active deployments"
 if [ ! -s .bootstrap/bootstrapped_deployments ]; then
    echo "  No active deployments found - proceeding with tests"
 else
     echo "  Active deployments found"
-    echo "  WARNING: Continuing this script will remove all existing deployments, including any unsaved data"
+    echo "  WARNING: Continuing this script will remove all existing Tyk Demo deployments, including any unsaved data"
     
     read -p "  Press enter to continue, or CTRL-C to exit"
     echo "Removing active deployments..."
-    $(./down.sh 1>/dev/null)
+    ./down.sh
 fi
 
 for dir in deployments/*/     
@@ -30,7 +27,7 @@ do
     postman_collection_file_name="tyk_demo_${deployment_name//-/_}.postman_collection.json"
     postman_collection_path="$deployment_dir/$postman_collection_file_name"
 
-    # Postman collections contain the tests. If the deployment doesn't have one then it can be skipped.
+    # If the deployment doesn't have a postman collection then there are no tests to perform, so the deployment can be skipped
     echo "  Checking for Postman collection ($postman_collection_file_name)"
 
     if [ -z "$(ls -A $postman_collection_path 2>/dev/null)" ]; then
@@ -40,11 +37,11 @@ do
         echo "    Collection found. Proceeding..."
     fi
 
-    echo "  Creating deployment..."
-    $(./up.sh $deployment_name 1>/dev/null)
+    echo "Creating deployment: $deployment_name"
+    ./up.sh $deployment_name
 
 
-    echo "  Performing tests..."
+    echo "Testing deployment: $deployment_name "
 
     # Provide the 'test' environment variables, so newman can target the correct hosts from within the docker network
     # --insecure option is used due to self-signed certificates
@@ -57,20 +54,23 @@ do
         --environment /etc/postman/test.postman_environment.json \
         --insecure
 
-    echo "  Removing deployment..."
-    $(./down.sh 1>/dev/null)
+    if [ "$?" != "0" ]; then
+        echo "Tests failed"
+        # exit 1;
+        # TODO: record which tests failed
+    else
+        echo "Tests passed"
+    fi        
 
-
-    # echo "    Checking for Postman collection"
-
-    # postman_file_name=${dir//-/}
-
-
-
-
+    echo "Removing deployment: $deployment_name"
+    ./down.sh
 done
 
 
+
+# TODO: 
+# - display test statuses for all deployments (pass/fail/none)
+# - exit with correct exit code
 
 
 # Stop on first error
@@ -86,18 +86,3 @@ function onExit {
 }
 
 trap onExit EXIT;
-
-
-
-
-
-# Provide the 'test' environment variables, so newman can target the correct hosts from within the docker network
-# --insecure option is used due to self-signed certificates
-# docker run -t --rm \
-#     --network tyk-demo_tyk \
-#     -v $(pwd)/deployments/tyk/tyk_demo.postman_collection.json:/etc/postman/tyk_demo.postman_collection.json \
-#     -v $(pwd)/test.postman_environment.json:/etc/postman/test.postman_environment.json \
-#     postman/newman:alpine \
-#     run "/etc/postman/tyk_demo.postman_collection.json" \
-#     --environment /etc/postman/test.postman_environment.json \
-#     --insecure
