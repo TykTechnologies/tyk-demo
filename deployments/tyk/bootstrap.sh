5c38b799d5135a342c366121cf2bca2a15d59727
David Garvey <davegarvey@hotmail.com>
2023-01-27 17:25:43 +0100

    debugging

--- deployments/tyk/bootstrap.sh
+++ deployments/tyk/bootstrap.sh
@@ -105,7 +105,10 @@ wait_for_response "$dashboard_base_url/admin/organisations" "200" "admin-auth: $
 # Python plugin
 
 log_message "Building Python plugin bundle"
-docker exec $(get_service_container_id tyk-gateway) sh -c "cd /opt/tyk-gateway/middleware/python/basic-example; /opt/tyk-gateway/tyk bundle build -k /opt/tyk-gateway/certs/private-key.pem" 1> /dev/null 2>> bootstrap.log
+gateway_docker_pwd=$(docker exec $(get_service_container_id tyk-gateway) pwd)
+log_message "  Gateway container working directory path: $gateway_docker_pwd"
+docker exec $(get_service_container_id tyk-gateway) sh -c "cd /opt/tyk-gateway/middleware/python/basic-example; /opt/tyk-gateway/tyk bundle build -k /opt/tyk-gateway/certs/private-key.pem"
+# 1> /dev/null 2>> bootstrap.log
 # eval "$(generate_docker_compose_command) exec -T -d tyk-gateway sh -c \"cd /opt/tyk-gateway/middleware/python/basic-example; /opt/tyk-gateway/tyk bundle build -k /opt/tyk-gateway/certs/private-key.pem\"" 1> /dev/null 2>> bootstrap.log
 if [ "$?" != 0 ]; then
   echo "Error occurred when building Python plugin bundle"
@@ -115,8 +118,6 @@ log_ok
 bootstrap_progress
 
 log_message "Copying Python bundle to http-server"
-gateway_docker_pwd=$(docker exec $(get_service_container_id tyk-gateway) pwd)
-log_message "  Gateway container working directory path: $gateway_docker_pwd"
 # we don't use a 'docker compose' command here as docker compose version 1 does not support 'cp'
 docker cp $(get_service_container_id tyk-gateway):$gateway_docker_pwd/middleware/python/basic-example/bundle.zip deployments/tyk/volumes/http-server/python-basic-example.zip 2>>bootstrap.log
 if [ "$?" != 0 ]; then
