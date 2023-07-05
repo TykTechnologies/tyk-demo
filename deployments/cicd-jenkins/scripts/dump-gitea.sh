cae617baf877ab3c2d413865caaa0d1b447b1caa
David Garvey <davegarvey@hotmail.com>
2023-07-05 18:09:19 +0200

    add sleep to allow command to execute correctly

--- deployments/cicd-jenkins/scripts/dump-gitea.sh
+++ deployments/cicd-jenkins/scripts/dump-gitea.sh
@@ -4,9 +4,13 @@ source scripts/common.sh
 
 # generates a Gitea archive dump of the configuration and repositories
 # then copies the file from the container and overwrites the tracked archive in this repo
+# note: script must be run from repo root i.e. run as ./deployments/cicd-jenkins/scripts/dump-gitea.sh
 
 check_docker_compose_version
 
-eval "$(generate_docker_compose_command) exec -d -u git gitea sh -c \"cd /data/gitea; rm gitea-dump.zip; gitea dump -c /data/gitea/conf/app.ini --f gitea-dump.zip\""
+$(generate_docker_compose_command) exec -d -u git gitea sh -c "cd /data/gitea; rm gitea-dump.zip; gitea dump -c /data/gitea/conf/app.ini --f gitea-dump.zip"
+
+# sleep 2 seconds to give time for the command issue by the previous statement to fully complete on the container
+sleep 2
 
 docker cp $(get_service_container_id gitea):/data/gitea/gitea-dump.zip deployments/cicd-jenkins/volumes/gitea/gitea-dump.zip
