dd20505e8c3000ae756bd5c341b3217036180f06
David Garvey <davegarvey@hotmail.com>
2023-08-30 16:39:20 +0200

    update syk sync version

--- deployments/cicd-jenkins/scripts/dump-tyk.sh
+++ deployments/cicd-jenkins/scripts/dump-tyk.sh
@@ -7,5 +7,5 @@ gitea_tyk_data_repo_path=$(cat .context-data/gitea-tyk-data-repo-path)
 docker run --rm \
   --network tyk-demo_tyk \
   -v $gitea_tyk_data_repo_path:/opt/tyk-sync/data \
-  tykio/tyk-sync:v1.1.0 \
+  tykio/tyk-sync:v1.4.0 \
   dump -d http://tyk-dashboard:3000 -s $dashboard_user_api_credentials -t data
