96cdf7bdde104853719fbc6a159ec4456845b079
David Garvey <davegarvey@hotmail.com>
2023-10-26 18:29:48 +0200

    minor amends

--- deployments/backstage/bootstrap.sh
+++ deployments/backstage/bootstrap.sh
@@ -11,10 +11,15 @@ dashboard_api_key=$(get_context_data "1" "dashboard-user" "1" "api-key")
 
 log_message "Updating Dashboard API definition to use current Dashboard API key"
 updated_api_data=$(jq --compact-output --raw-output --arg dashboard_api_key "$dashboard_api_key" '.api_definition.version_data.versions.Default.global_headers.Authorization = $dashboard_api_key' deployments/backstage/data/tyk-dashboard/api-653a7e6942033d00015b9059.json)
+log_message "  Ok"
+
+log_message "Adding updated Dashboard API definition to Tyk"
 log_json_result "$(curl $dashboard_base_url/api/apis -s \
     -H "Authorization: $dashboard_api_key" \
     -d "$updated_api_data")"
 
 bootstrap_progress
-
 log_end_deployment
+
+# blank output to overwrite "bootstrapping..." message
+echo -e "\033[2K "
