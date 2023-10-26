f27b49746945c0a42e0579a6b8370662da0c1c5e
David Garvey <davegarvey@hotmail.com>
2023-10-26 18:25:32 +0200

    import API def for backstage

--- deployments/backstage/bootstrap.sh
+++ deployments/backstage/bootstrap.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-# TODO:
-# - move backstage api def into deployment dir
-# - automate setting of authz header in api def
+source scripts/common.sh
+deployment="Backstage"
+
+log_start_deployment
+bootstrap_progress
+
+dashboard_base_url="http://tyk-dashboard.localhost:3000"
+dashboard_api_key=$(get_context_data "1" "dashboard-user" "1" "api-key")
+
+log_message "Updating Dashboard API definition to use current Dashboard API key"
+updated_api_data=$(jq --compact-output --raw-output --arg dashboard_api_key "$dashboard_api_key" '.api_definition.version_data.versions.Default.global_headers.Authorization = $dashboard_api_key' deployments/backstage/data/tyk-dashboard/api-653a7e6942033d00015b9059.json)
+log_json_result "$(curl $dashboard_base_url/api/apis -s \
+    -H "Authorization: $dashboard_api_key" \
+    -d "$updated_api_data")"
+
+bootstrap_progress
+
+log_end_deployment
