8ea0ff20488553537f446330efc6beb10289904d
David Garvey <davegarvey@hotmail.com>
2023-12-21 12:38:15 +0100

    update to 5.2.5

--- scripts/add-gateway.sh
+++ scripts/add-gateway.sh
@@ -8,4 +8,4 @@ docker run \
   -v $(pwd)/deployments/tyk/volumes/tyk-gateway/tyk.conf:/opt/tyk-gateway/tyk.conf \
   -v $(pwd)/deployments/tyk/volumes/tyk-gateway/middleware:/opt/tyk-gateway/middleware \
   --network tyk-demo_tyk \
-  tykio/tyk-gateway:latest
+  tykio/tyk-gateway:v5.2.5
