b29582b5ce172c6dbb1dedb50b5b3e03a41fe7a9
David Garvey <davegarvey@hotmail.com>
2024-01-05 14:30:11 +0100

    comment

--- deployments/backstage/bootstrap.sh
+++ deployments/backstage/bootstrap.sh
@@ -10,8 +10,9 @@ dashboard_base_url="http://tyk-dashboard.localhost:3000"
 dashboard_api_key=$(get_context_data "1" "dashboard-user" "1" "api-key")
 dashboard_admin_api_credentials=$(cat deployments/tyk/volumes/tyk-dashboard/tyk_analytics.conf | jq -r .admin_secret 2>> logs/bootstrap.log)
 
+# this assumes that the backstage backend is available on the host machine on port 7007, and that it has the tyk entitiy provider installed
 log_message "Adding API event webhook to default organisation"
-updated_org_data=$(jq '.event_options += { "api_event": { "webhook": "http://host.docker.internal:7007/api/catalog/tyk/api/hook", "email": "", "redis": false } }' < deployments/tyk/data/tyk-dashboard/1/organisation.json)
+updated_org_data=$(jq '.event_options += { "api_event": { "webhook": "http://host.docker.internal:7007/tyk/api/import-all", "email": "", "redis": false } }' < deployments/tyk/data/tyk-dashboard/1/organisation.json)
 api_response=$(curl $dashboard_base_url/admin/organisations/5e9d9544a1dcd60001d0ed20 --request PUT -s \
     -H "admin-auth: $dashboard_admin_api_credentials" \
     -d "$updated_org_data" 2>> logs/bootstrap.log)
