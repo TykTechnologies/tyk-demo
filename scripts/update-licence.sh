e8e3caf26939579bb793d71cbbe83315f645ec84
Yaara <letz.yaara@gmail.com>
2024-01-08 17:36:45 +0000

    update license script

---
+++ scripts/update-licence.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#
+# Prompt user for the new license key
+#
+if [ -z "$1" ]; then
+    echo "Please provide the new license key as an argument."
+    exit 1
+fi
+
+new_license_key="$1"
+
+# Save copy before the update
+cp .env .env.bak
+
+# Update the DASHBOARD_LICENCE in .env with the new license key
+sed -e "s/^DASHBOARD_LICENCE=.*$/DASHBOARD_LICENCE=${new_license_key}/" .env > .env.output && mv .env.output .env
+
+# Check if sed command succeeded
+if [ $? -eq 0 ]; then
+    echo "License key updated successfully"
+else
+    echo "Failed to update the license key"
+fi
