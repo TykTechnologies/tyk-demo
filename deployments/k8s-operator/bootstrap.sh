06bfa73ab1de757abe2620677b1116f9f96895b8
David Garvey <davegarvey@hotmail.com>
2024-02-09 21:15:03 +0100

    wip

--- deployments/k8s-operator/bootstrap.sh
+++ deployments/k8s-operator/bootstrap.sh
@@ -3,6 +3,7 @@
 source scripts/common.sh
 
 namespace="tyk-operator-system"
+set_context_data "1" "operator" "1" "namespace" "$namespace"
 
 echo "Installing cert-manager"
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.yaml
@@ -17,3 +18,20 @@ helm install tyk-operator tyk-helm/tyk-operator -n $namespace
 
 echo "Creating Tyk Operator configuration"
 ./scripts/setup-operator-secrets.sh $namespace
+
+# echo "Creating an example API CRD"
+# cat <<EOF | kubectl apply -f -
+# apiVersion: tyk.tyk.io/v1alpha1
+# kind: ApiDefinition
+# metadata:
+#   name: operator-httpbin
+# spec:
+#   name: Operator httpbin
+#   use_keyless: true
+#   protocol: http
+#   active: true
+#   proxy:
+#     target_url: http://httpbin
+#     listen_path: /operator-httpbin
+#     strip_listen_path: true
+# EOF
