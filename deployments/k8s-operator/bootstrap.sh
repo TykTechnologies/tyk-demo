0be387fd7ef4999f85ffcf25579ab164d0e3c271
David Garvey <davegarvey@hotmail.com>
2024-02-09 17:22:09 +0100

    further work on bootstrap

--- deployments/k8s-operator/bootstrap.sh
+++ deployments/k8s-operator/bootstrap.sh
@@ -2,14 +2,18 @@
 
 source scripts/common.sh
 
-echo "Installing cert-manager"
+namespace="tyk-operator-system"
 
+echo "Installing cert-manager"
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+
+echo "Creating Tyk Operator namespace"
+kubectl create namespace $namespace
 
 echo "Installing Tyk Operator"
 helm repo add tyk-helm https://helm.tyk.io/public/helm/charts/
 helm repo update
-helm install tyk-operator tyk-helm/tyk-operator -n tyk-operator-system
+helm install tyk-operator tyk-helm/tyk-operator -n $namespace
 
-echo "Creating Tyk Operator configuration
-./scripts/setup-operator-secrets.sh
+echo "Creating Tyk Operator configuration"
+./scripts/setup-operator-secrets.sh $namespace
