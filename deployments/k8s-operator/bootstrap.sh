cafe8da1f5560a37164d95346084a1e33426832c
David Garvey <davegarvey@hotmail.com>
2024-02-09 17:03:24 +0100

    add bootstrap script

--- deployments/k8s-operator/bootstrap.sh
+++ deployments/k8s-operator/bootstrap.sh
@@ -2,3 +2,14 @@
 
 source scripts/common.sh
 
+echo "Installing cert-manager"
+
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+
+echo "Installing Tyk Operator"
+helm repo add tyk-helm https://helm.tyk.io/public/helm/charts/
+helm repo update
+helm install tyk-operator tyk-helm/tyk-operator -n tyk-operator-system
+
+echo "Creating Tyk Operator configuration
+./scripts/setup-operator-secrets.sh
