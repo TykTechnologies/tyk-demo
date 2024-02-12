d22928a5c07af320011f5e59634295d39079b3b8
David Garvey <davegarvey@hotmail.com>
2024-02-12 11:33:58 +0100

    switch certmanager to helm

--- deployments/k8s-operator/bootstrap.sh
+++ deployments/k8s-operator/bootstrap.sh
@@ -1,37 +1,31 @@
 #!/bin/bash
 
+# TODO:
+# - send messages to bootstrap log
+# - fix deployment issue (perhaps need to status check between commands, to ensure services are ready)
+
 source scripts/common.sh
 
 namespace="tyk-operator-system"
 set_context_data "1" "operator" "1" "namespace" "$namespace"
 
 echo "Installing cert-manager"
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+helm repo add jetstack https://charts.jetstack.io --force-update
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.8.0 \
+  --set installCRDs=true
 
 echo "Creating Tyk Operator namespace"
 kubectl create namespace $namespace
 
 echo "Installing Tyk Operator"
-helm repo add tyk-helm https://helm.tyk.io/public/helm/charts/
+helm repo add tyk-helm https://helm.tyk.io/public/helm/charts/ --force-update
 helm repo update
 helm install tyk-operator tyk-helm/tyk-operator -n $namespace
 
 echo "Creating Tyk Operator configuration"
 ./scripts/setup-operator-secrets.sh $namespace
-
-# echo "Creating an example API CRD"
-# cat <<EOF | kubectl apply -f -
-# apiVersion: tyk.tyk.io/v1alpha1
-# kind: ApiDefinition
-# metadata:
-#   name: operator-httpbin
-# spec:
-#   name: Operator httpbin
-#   use_keyless: true
-#   protocol: http
-#   active: true
-#   proxy:
-#     target_url: http://httpbin
-#     listen_path: /operator-httpbin
-#     strip_listen_path: true
-# EOF
--- deployments/k8s-operator/teardown.sh
+++ deployments/k8s-operator/teardown.sh
@@ -4,6 +4,14 @@ source scripts/common.sh
 
 namespace=$(get_context_data "1" "operator" "1" "namespace")
 
+if [ "$namespace" == "" ]; then
+    namespace="tyk-operator-system"
+fi
+
+# TODO: check if exists
+echo "Deleting example API CRD" 
+kubectl delete tykapis operator-httpbin
+
 echo "Uninstalling Tyk Operator"
 helm uninstall tyk-operator -n $namespace
 
@@ -13,8 +21,8 @@ kubectl delete secret/tyk-operator-conf -n $namespace
 echo "Deleting Tyk Operator namespace"
 kubectl delete namespace $namespace
 
-echo "Deleting example API CRD"
-kubectl delete tykapis operator-httpbin
-
 echo "Deleting cert-manager"
-kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.yaml
+helm --namespace cert-manager delete cert-manager
+kubectl delete namespace cert-manager
+# this command likely creates errors
+kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.8.0/cert-manager.crds.yaml
