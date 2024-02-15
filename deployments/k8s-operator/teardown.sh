83fccf79045eb08ee6844d59c9a0a8f835b6cc2d
David Garvey <davegarvey@hotmail.com>
2024-02-15 21:14:53 +0100

    remove all tykapis in teardown

--- deployments/k8s-operator/teardown.sh
+++ deployments/k8s-operator/teardown.sh
@@ -12,19 +12,16 @@ if [ "$namespace" == "" ]; then
     namespace="tyk-demo"
 fi
 
-log_message "Removing example API CRD" 
-kubectl get -n $namespace -f deployments/k8s-operator/data/tyk-operator/httpbin.yaml 1>/dev/null 2>>logs/bootstrap.log
-if [[ "$?" == 0 ]]; then
-    log_message "  Example API CRD found - deleting" 
-    kubectl delete -n $namespace -f deployments/k8s-operator/data/tyk-operator/httpbin.yaml 1>/dev/null 2>>logs/bootstrap.log
+log_message "Removing API CRDs" 
+kubectl get tykapis -n tyk-demo --no-headers -o custom-columns=":metadata.name" | while read -r api_name; do
+    log_message "  Removing API: $api_name"
+    kubectl delete -n $namespace tykapis/$api_name 1>/dev/null 2>>logs/bootstrap.log
     if [[ "$?" != "0" ]]; then
-        log_message "ERROR: Unable to delete example API CRD"
+        log_message "ERROR: Unable to delete $api_name API CRD"
     else
         log_ok
     fi
-else
-    log_message "  Example API CRD not found - skipping"
-fi
+done
 
 log_message "Uninstalling Tyk Operator"
 helm uninstall tyk-operator -n $namespace 1>/dev/null 2>>logs/bootstrap.log
