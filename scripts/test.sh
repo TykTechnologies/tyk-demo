47c16be66bbf181aca7878a346406cc653f76f00
David Garvey <davegarvey@hotmail.com>
2024-03-22 10:27:26 +0100

    perform tests on all bootstrapped deployments

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# runs the postman collection tests for deployments that a currently deployed
+
 # Stop on first error
 set -e;
 
@@ -14,13 +16,18 @@ function onExit {
 
 trap onExit EXIT;
 
-# Provide the 'test' environment variables, so newman can target the correct hosts from within the docker network
-# --insecure option is used due to self-signed certificates
-docker run -t --rm \
-    --network tyk-demo_tyk \
-    -v $(pwd)/deployments/tyk/tyk_demo_tyk.postman_collection.json:/etc/postman/tyk_demo.postman_collection.json \
-    -v $(pwd)/test.postman_environment.json:/etc/postman/test.postman_environment.json \
-    postman/newman:alpine \
-    run "/etc/postman/tyk_demo.postman_collection.json" \
-    --environment /etc/postman/test.postman_environment.json \
-    --insecure
+# loop through bootstrapped deployments
+while IFS= read -r deployment; do
+    echo "Running tests for $deployment deployment"
+
+    # Provide the 'test' environment variables, so newman can target the correct hosts from within the docker network
+    # --insecure option is used due to self-signed certificates
+    docker run -t --rm \
+        --network tyk-demo_tyk \
+        -v $(pwd)/deployments/$deployment/tyk_demo_$deployment.postman_collection.json:/etc/postman/tyk_demo.postman_collection.json \
+        -v $(pwd)/test.postman_environment.json:/etc/postman/test.postman_environment.json \
+        postman/newman:alpine \
+        run "/etc/postman/tyk_demo.postman_collection.json" \
+        --environment /etc/postman/test.postman_environment.json \
+        --insecure
+done < .bootstrap/bootstrapped_deployments
