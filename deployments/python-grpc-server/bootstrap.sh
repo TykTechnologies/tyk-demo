811a34fb922dc158e6dfa90e829209c85b43c51d
Simon Pears <simon@tyk.io>
2024-05-01 09:41:30 +0100

    update grpc deployment bootstrap to load api

--- deployments/python-grpc-server/bootstrap.sh
+++ deployments/python-grpc-server/bootstrap.sh
@@ -6,6 +6,29 @@ deployment="Python-gRPC-Server"
 log_start_deployment
 bootstrap_progress
 
+# Setup variables
+dashboard_base_url="http://tyk-dashboard.localhost:$(jq -r '.listen_port' deployments/tyk/volumes/tyk-dashboard/tyk_analytics.conf)"
+gateway_base_url="http://$(jq -r '.host_config.override_hostname' deployments/tyk/volumes/tyk-dashboard/tyk_analytics.conf)"
+gateway_api_credentials=$(cat deployments/tyk/volumes/tyk-gateway/tyk.conf | jq -r .secret)
+dashboard_admin_api_credentials=$(cat deployments/tyk/volumes/tyk-dashboard/tyk_analytics.conf | jq -r .admin_secret)
+dashboard_user_api_key=$(get_context_data "1" "dashboard-user" "1" "api-key")
+
+# Create custom-auth API for Python gRCP
+create_api "deployments/python-grpc-server/data/apis-python_grpc.json" "$dashboard_admin_api_credentials" "$dashboard_user_api_key"
+bootstrap_progress
+
+# Check that the API has loaded
+log_message "Waiting for API availability"
+for file in deployments/python-grpc-server/data/*; do
+  if [[ -f $file ]]; then
+    target_api_id=$(cat $file | jq '.api_definition.api_id' --raw-output)
+    wait_for_api_loaded "$target_api_id" "$gateway_base_url" "$gateway_api_credentials"
+    bootstrap_progress        
+  fi
+done
+bootstrap_progress        
+log_ok
+
 log_end_deployment
 
 echo -e "\033[2K
