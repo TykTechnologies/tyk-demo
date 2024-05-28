cb7f54f7c68d44a44925b9d95657152da195a080
David Garvey <davegarvey@hotmail.com>
2024-05-28 16:54:34 +0200

    improve efficiency

--- scripts/recreate-gateways.sh
+++ scripts/recreate-gateways.sh
@@ -2,7 +2,13 @@
 
 source scripts/common.sh
 
+service_names=""
+
 for container_id in $(docker ps -q --filter name=tyk-gateway); do
     service_name=$(docker inspect --format '{{index .Config.Labels "com.docker.compose.service"}}' $container_id)
-    eval $(generate_docker_compose_command) up -d --no-deps --force-recreate $service_name
+    service_names="$service_names $service_name"
 done
+
+if [ -n "$service_names" ]; then
+    eval $(generate_docker_compose_command) up -d --no-deps --force-recreate $service_names
+fi
