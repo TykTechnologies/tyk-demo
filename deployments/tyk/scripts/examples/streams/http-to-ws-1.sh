e39321c5f08fe9ecb331106444b37d059bc34cea
David Garvey <davegarvey@hotmail.com>
2024-07-19 16:50:10 +0200

    add scripted examples for http to ws

---
+++ deployments/tyk/scripts/examples/streams/http-to-ws-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Streams - HTTP to Websocket Example"
+echo "Part 1: Listening to a Websocket"
+echo "Once connected, run part two of this example in a different terminal. The messages sent will appear here."
+echo "Connecting to the websocket..."
+wscat -c ws://tyk-gateway.localhost:8080/streams-http-to-websocket/subscribe
