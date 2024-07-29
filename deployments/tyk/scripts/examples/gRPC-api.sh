e4b3f6b7cc3a9d0e4e7a6f424596971ad64fae6a
David Garvey <davegarvey@hotmail.com>
2024-07-29 17:51:59 +0200

    example script for grpc services

---
+++ deployments/tyk/scripts/examples/gRPC-api.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This example uses grpcurl to call gRPC API services proxied by the Tyk gateway.
+
+# Check if grpcurl is installed
+if ! command -v grpcurl &> /dev/null
+then
+    echo "grpcurl is not installed. Please install grpcurl to proceed:"
+    echo "brew install grpcurl"
+    exit 1
+fi
+
+echo "Available services:"
+grpcurl -plaintext tyk-gateway.localhost:9002 list
+
+echo -e "\nExample 1: ralph greeting - using gRPC without TLS"
+grpcurl -plaintext -d '{"greeting":"ralph"}' tyk-gateway.localhost:9002 hello.HelloService.SayHello
+
+echo -e "\nExample 2: maus greeting - using gRPC with TLS"
+# -insecure used to ignore self-signed certificate
+grpcurl -insecure -d '{"greeting":"maus"}' tyk-gateway.localhost:9003 hello.HelloService.SayHello
+
+echo -e "\nExample 3: maths - 1 + 2 = ?"
+grpcurl -plaintext -d '{"a":1,"b":2}' tyk-gateway.localhost:9002 addsvc.Add.Sum
