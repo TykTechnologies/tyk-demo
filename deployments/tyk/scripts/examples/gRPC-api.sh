41a0a36ad42c99e279894a4eb1060c86290684c5
David Garvey <davegarvey@hotmail.com>
2024-07-29 17:57:06 +0200

    add note on ports

--- deployments/tyk/scripts/examples/gRPC-api.sh
+++ deployments/tyk/scripts/examples/gRPC-api.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # This example uses grpcurl to call gRPC API services proxied by the Tyk gateway.
+# Port 9002 proxies to insecure gRPC (note -plaintext flag)
+# Port 9003 proxies to secure gRPC (note -insecure flag, due to self-signed certificate)
 
 # Check if grpcurl is installed
 if ! command -v grpcurl &> /dev/null
@@ -17,7 +19,6 @@ echo -e "\nExample 1: ralph greeting - using gRPC without TLS"
 grpcurl -plaintext -d '{"greeting":"ralph"}' tyk-gateway.localhost:9002 hello.HelloService.SayHello
 
 echo -e "\nExample 2: maus greeting - using gRPC with TLS"
-# -insecure used to ignore self-signed certificate
 grpcurl -insecure -d '{"greeting":"maus"}' tyk-gateway.localhost:9003 hello.HelloService.SayHello
 
 echo -e "\nExample 3: maths - 1 + 2 = ?"
