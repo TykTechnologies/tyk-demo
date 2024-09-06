e704d112e629284530782690d869ec162b85ad92
David Garvey <davegarvey@hotmail.com>
2024-09-06 10:12:25 +0200

    minor improvements

--- deployments/tyk/scripts/examples/grpc/gRPC-tcp-multiplex.sh
+++ deployments/tyk/scripts/examples/grpc/gRPC-tcp-multiplex.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# This example uses grpcurl to call a gRPC API service proxied by the Tyk gateway using TLS.
-# The Gateway is listening for both APIs on the same port (9005), so it uses the SNI data to find the correct API definition based on its custom domain (tls-multiplex-1.localhost / tls-multiplex-2.localhost).
+# This example uses grpcurl to call different TLS API definitions that are configured to listen on the same port (9005).
+# The Gateway uses the SNI data to find the correct API definition based on its custom domain (tls-multiplex-1.localhost / tls-multiplex-2.localhost).
 
 source scripts/common.sh
 
 check_for_grpcurl
 
-echo "Sending gRPC request via tls-multiplex-1.localhost"
+echo "Sending gRPC request via tls-multiplex-1.localhost: this request should work, as the API definition is active"
 grpcurl -insecure -d '{"greeting":"Maus"}' tls-multiplex-1.localhost:9005 hello.HelloService.SayHello
-echo "Sending gRPC request via tls-multiplex-2.localhost"
+echo "Sending gRPC request via tls-multiplex-2.localhost: this request should fail by timeout, as the API definition target URL does not exist"
 grpcurl -insecure -d '{"greeting":"Ralph"}' tls-multiplex-2.localhost:9005 hello.HelloService.SayHello
