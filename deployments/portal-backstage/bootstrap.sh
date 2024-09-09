2a761b0d3e8838dd05beaedaf2dfc58ca06d62c1
David Garvey <davegarvey@hotmail.com>
2024-09-09 19:40:21 +0200

    wip

--- deployments/portal-backstage/bootstrap.sh
+++ deployments/portal-backstage/bootstrap.sh
@@ -2,3 +2,13 @@
 
 source scripts/common.sh
 
+deployment="Backstage"
+log_start_deployment
+
+log_end_deployment
+
+
+echo -e "\033[2K
+▼ Portal - Backstage
+  ▽ Backstage Dashboard
+          Dashboard URL : http://localhost:3003"
