e41c68a4935be9280d3b067da591e8b4374f8638
David Garvey <davegarvey@hotmail.com>
2024-09-10 16:57:57 +0200

    configure webhook callback to entity provider router endpoint

--- deployments/portal-backstage/bootstrap.sh
+++ deployments/portal-backstage/bootstrap.sh
@@ -5,8 +5,19 @@ source scripts/common.sh
 deployment="Backstage"
 log_start_deployment
 
+dashboard_base_url="http://tyk-dashboard.localhost:3000"
+dashboard_api_key=$(get_context_data "1" "dashboard-user" "1" "api-key")
+dashboard_admin_api_credentials=$(cat deployments/tyk/volumes/tyk-dashboard/tyk_analytics.conf | jq -r .admin_secret 2>> logs/bootstrap.log)
+
+log_message "Adding API event webhook to default organisation"
+updated_org_data=$(jq '.event_options += { "api_event": { "webhook": "http://backstage:7007/api/catalog/tyk/development/sync" } }' < deployments/tyk/data/tyk-dashboard/1/organisation.json)
+api_response=$(curl $dashboard_base_url/admin/organisations/5e9d9544a1dcd60001d0ed20 --request PUT -s \
+    -H "admin-auth: $dashboard_admin_api_credentials" \
+    -d "$updated_org_data" 2>> logs/bootstrap.log)
+log_json_result "$api_response"
+
 log_message "Writing Tyk Dashboard API access token to .env file"
-set_docker_environment_value "TYK_DASHBOARD_API_ACCESS_CREDENTIALS" $(cat .context-data/1-dashboard-user-1-api-key)
+set_docker_environment_value "TYK_DASHBOARD_API_ACCESS_CREDENTIALS" "$dashboard_api_key"
 log_ok
 
 log_message "Restarting Backstage container to use new access token env var value"
