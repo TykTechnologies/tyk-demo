e47ad28c44d139efc95c92184742e12a6a7dd46c
David Garvey <davegarvey@hotmail.com>
2024-10-04 10:29:08 +0200

    update echo message

--- deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-1.sh
+++ deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-1.sh
@@ -1,6 +1,6 @@
 echo "Streams - Simple PubSub"
 echo "Part 1: Using server-sent events"
 echo "This script connects to a server-sent event endpoint, and waits to receive messages."
-echo "Messages are sent by script 3."
+echo "Use script 3 to send messages."
 
 curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/sse
--- deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-2.sh
+++ deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-2.sh
@@ -7,6 +7,6 @@ check_for_wscat
 echo "Streams - Simple PubSub"
 echo "Part 2: Using websockets"
 echo "This script connects to a websocket endpoint, and waits to receive messages."
-echo "Messages are sent by script 3."
+echo "Use script 3 to send messages."
 
 wscat -c ws://tyk-gateway.localhost:8080/streams-simple-pubsub/ws
