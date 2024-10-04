15f9e44c8d9f6990727905cceb8c0f5ce85be31c
David Garvey <davegarvey@hotmail.com>
2024-10-04 16:54:29 +0200

    text updates

--- deployments/tyk/scripts/examples/streams/validated-pubsub/validated-pubsub-1.sh
+++ deployments/tyk/scripts/examples/streams/validated-pubsub/validated-pubsub-1.sh
@@ -1,6 +1,6 @@
 echo "Streams - Validated PubSub"
 echo "Part 1: Listening for validated server-sent events"
 echo "This script connects to a server-sent event endpoint, and waits to receive messages."
-echo "Use script 3 to send messages."
+echo "Use script 2 to send a valid message that is received here."
 
 curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/validated-sse
