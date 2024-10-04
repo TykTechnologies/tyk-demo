477fe2ee7aaa28fb045ff6c7ef9cfcc2ec57d131
David Garvey <davegarvey@hotmail.com>
2024-10-04 17:13:59 +0200

    minor amends

--- deployments/tyk/scripts/examples/streams/validated-pubsub/validated-pubsub-2.sh
+++ deployments/tyk/scripts/examples/streams/validated-pubsub/validated-pubsub-2.sh
@@ -3,4 +3,4 @@ echo "Part 2: Publishing a valid message"
 echo "This script publishes a valid message to the clients connected in script 1."
 echo "Check the first terminal to see the messages received."
 
-curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/validated-pub -d '{ "name": "Tyk Streams", "age": 21 }'
+curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/validated-pub -d '{ "name": "Tyk Streams", "age": 1 }'
--- deployments/tyk/scripts/examples/streams/validated-pubsub/validated-pubsub-3.sh
+++ deployments/tyk/scripts/examples/streams/validated-pubsub/validated-pubsub-3.sh
@@ -3,4 +3,4 @@ echo "Part 3: Publishing an invalid message"
 echo "This script publishes an invalid message, which the gateway prevents subscribers from receiving."
 echo "Check the first terminal to see that the message is not received, and the fourth terminal to see the logged error."
 
-curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/validated-pub -d '{ "name": "Tyk", "age": 3 }'
+curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/validated-pub -d '{ "name": "Tyk Streams", "age": -1 }'
