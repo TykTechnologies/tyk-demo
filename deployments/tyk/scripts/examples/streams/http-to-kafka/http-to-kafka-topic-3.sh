1f6b5472a789fa29cb4c4997d52eaeca65d8c34d
David Garvey <davegarvey@hotmail.com>
2024-10-07 14:03:10 +0200

    capitalisation

--- deployments/tyk/scripts/examples/streams/http-to-kafka/http-to-kafka-topic-3.sh
+++ deployments/tyk/scripts/examples/streams/http-to-kafka/http-to-kafka-topic-3.sh
@@ -1,5 +1,5 @@
 echo "Streams - HTTP to Kakfa"
 echo "Part 3: Send messages to the Kafka topic via HTTP"
 echo "This sends a messages to the 'tyk-streams-example' Kafka topic via an HTTP request that is handled by Tyk."
-echo "Now check the first and second terminals to see that the message 'Hello, Tyk streams!' has appeared."
-curl -X POST http://tyk-gateway.localhost:8080/streams-http-to-kafka/post -d 'Hello, Tyk streams!'
+echo "Now check the first and second terminals to see that the message 'Hello, Tyk Streams!' has appeared."
+curl -X POST http://tyk-gateway.localhost:8080/streams-http-to-kafka/post -d 'Hello, Tyk Streams!'
