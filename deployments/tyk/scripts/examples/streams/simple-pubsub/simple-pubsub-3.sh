4e2b7a270491f63cdefc8f522965f9d74123fa10
David Garvey <davegarvey@hotmail.com>
2024-10-07 14:04:32 +0200

    capitalisation

--- deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-3.sh
+++ deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-3.sh
@@ -2,4 +2,4 @@ echo "Streams - Simple PubSub"
 echo "Part 3: Sending messages to connected clients"
 echo "This script sends messages to the clients connected in scripts 1 and 2. Check their terminals to see the messages received."
 
-curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/pub -d 'Hello, Tyk streams!'
+curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/pub -d 'Hello, Tyk Streams!'
