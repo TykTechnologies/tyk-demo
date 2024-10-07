985f890c6e848fe9dec7f9a9497ef63f12e59e67
David Garvey <davegarvey@hotmail.com>
2024-10-07 13:57:48 +0200

    update messages

--- deployments/tyk/scripts/examples/streams/http-to-kafka/http-to-kafka-topic-1.sh
+++ deployments/tyk/scripts/examples/streams/http-to-kafka/http-to-kafka-topic-1.sh
@@ -1,5 +1,5 @@
 echo "Streams - HTTP to Kakfa"
-echo "Part 1: Consuming the Kafka topic"
-echo "This script connects to the 'tyk-streams-example' Kafka topic and waits for messages."
+echo "Part 1: Consume a Kafka topic via the Kafka console consumer"
+echo "This example connects to the 'tyk-streams-example' Kafka topic and waits for messages."
 echo "Now run part two in a different terminal, to connect to Kafka using Tyk Streams."
 docker exec -it tyk-demo-kafka-1 /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic tyk-streams-example --from-beginning
