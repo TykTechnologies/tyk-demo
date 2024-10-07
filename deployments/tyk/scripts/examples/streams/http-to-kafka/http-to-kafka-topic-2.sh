b6296f1a8e251d95a6c7f424b6c262e2061d6704
David Garvey <davegarvey@hotmail.com>
2024-10-07 13:52:54 +0200

    add kafka subscriber example

--- deployments/tyk/scripts/examples/streams/http-to-kafka/http-to-kafka-topic-2.sh
+++ deployments/tyk/scripts/examples/streams/http-to-kafka/http-to-kafka-topic-2.sh
@@ -1,5 +1,5 @@
 echo "Streams - HTTP to Kakfa"
-echo "Part 2: Send messages to the Kafka topic via HTTP"
-echo "This sends a messages to the 'tyk-streams-example' Kafka topic via an HTTP request that is handled by Tyk."
-echo "Now check the first terminal to see that the message 'Hello, Tyk streams!' has appeared."
-curl -X POST http://tyk-gateway.localhost:8080/streams-http-to-kafka/post -d 'Hello, Tyk streams!'
+echo "Part 2: Listen for Kafka messages via Tyk Streams"
+echo "This connects to Kafka via a Tyk Streams endpoint, which delivers the messages via server-sent events."
+echo "Now run part three of this example in a different terminal, to see the message appear here."
+curl http://tyk-gateway.localhost:8080/streams-http-to-kafka/sub
