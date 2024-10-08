88b297cf7934e4061c00168cb3a4a17afab951a8
David Garvey <davegarvey@hotmail.com>
2024-10-08 08:17:08 +0200

    grammar

--- deployments/tyk/scripts/examples/dynamic-client-mtls.sh
+++ deployments/tyk/scripts/examples/dynamic-client-mtls.sh
@@ -2,7 +2,7 @@
 
 # Run this script from the repo root e.g. ./deployments/tyk/scripts/examples/dynamic-client-mtls.sh
 
-# The script calls an API is configured to allow client certificate authentication.
+# The script calls an API that's configured to allow client certificate authentication.
 # The public key is registered in the dashboard, along with an API key, which is associated with the certificate and provides the access rights to the API.
 # The gateway uses the certificate provided in the request to match the API key and authorise access to the requested endpoint.
 # The -k flag is used to ignore certificate error associated with the gateway's self-signed certificate.
