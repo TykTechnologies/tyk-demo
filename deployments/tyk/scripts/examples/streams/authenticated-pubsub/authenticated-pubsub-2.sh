0c4c759887fb8bbbf7dd2604d1f314bae2291018
David Garvey <davegarvey@hotmail.com>
2024-10-10 14:43:35 +0200

    change http server endpoint to `post`

--- deployments/tyk/scripts/examples/streams/authenticated-pubsub/authenticated-pubsub-2.sh
+++ deployments/tyk/scripts/examples/streams/authenticated-pubsub/authenticated-pubsub-2.sh
@@ -2,6 +2,6 @@ echo "Streams - Authenticated PubSub"
 echo "Part 2: Authenticate as a publisher"
 echo "This script sends messages to the clients connected in script 1 - check its terminal to see the messages received."
 
-curl http://tyk-gateway.localhost:8080/streams-authenticated-pubsub/pub \
+curl http://tyk-gateway.localhost:8080/streams-authenticated-pubsub/post \
     -d 'Hello, Tyk streams!' \
     -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwicG9sIjoiNjZmZmI5OTMyYTRhNTEwMDAxODZkMzc0IiwiaWF0IjoxNTE2MjM5MDIyfQ.G_rM5NWQIQopOZABTLkZEfnp2XSAiN5vFyRZAOWbtFs'
--- deployments/tyk/scripts/examples/streams/authenticated-pubsub/authenticated-pubsub-3.sh
+++ deployments/tyk/scripts/examples/streams/authenticated-pubsub/authenticated-pubsub-3.sh
@@ -2,5 +2,5 @@ echo "Streams - Authenticated PubSub"
 echo "Part 3: Authentication failure"
 echo "This script attempts to send an unauthenticated message to the auth-protected endpoint. This results in the request being rejected."
 
-curl http://tyk-gateway.localhost:8080/streams-authenticated-pubsub/pub \
+curl http://tyk-gateway.localhost:8080/streams-authenticated-pubsub/post \
     -d 'Hello, Tyk streams!'
