4cdad8d5bda47018a49ccc5b8f6b857bcedbc6fc
David Garvey <davegarvey@hotmail.com>
2024-11-06 14:49:58 +0100

    messaging update

--- deployments/tyk/scripts/examples/streams/http-to-amqp/http-to-amqp-1.sh
+++ deployments/tyk/scripts/examples/streams/http-to-amqp/http-to-amqp-1.sh
@@ -1,6 +1,6 @@
 echo "Streams - HTTP to AMQP"
 echo "Part 1: Listen for messages"
-echo "Once connected, run part two of this example in a different terminal. The messages sent will appear here."
-echo "Tyk enables server-sent events to consume messages from an AMQP queue."
+echo "Once connected, run part two of this example in a different terminal. The messages sent by part 2 will appear here."
+echo "Tyk enables messages in am AMQP queue to be consumed using server-sent events."
 echo "Connecting to the SSE endpoint..."
 curl -N http://tyk-gateway.localhost:8080/streams-http-to-amqp/sse
