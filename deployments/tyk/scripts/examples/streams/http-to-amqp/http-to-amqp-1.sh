c7ccae2e9fb270152b716ccf511b82559d417c97
David Garvey <davegarvey@hotmail.com>
2024-11-06 14:47:27 +0100

    change to use SSE

--- deployments/tyk/scripts/examples/streams/http-to-amqp/http-to-amqp-1.sh
+++ deployments/tyk/scripts/examples/streams/http-to-amqp/http-to-amqp-1.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-source scripts/common.sh
-
-check_for_wscat
-
 echo "Streams - HTTP to AMQP"
 echo "Part 1: Listen for messages"
 echo "Once connected, run part two of this example in a different terminal. The messages sent will appear here."
-echo "Tyk enables the websocket to consume messages from the AMQP queue."
-echo "Connecting to the websocket..."
-wscat -c ws://tyk-gateway.localhost:8080/streams-http-to-amqp/ws
+echo "Tyk enables server-sent events to consume messages from an AMQP queue."
+echo "Connecting to the SSE endpoint..."
+curl -N http://tyk-gateway.localhost:8080/streams-http-to-amqp/sse
