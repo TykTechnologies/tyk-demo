266cf11d5e9d707c968631287be163aa7d2d36cc
David Garvey <davegarvey@hotmail.com>
2024-12-13 13:12:09 +0100

    add grafana to terminal output + change default password

--- deployments/healthcheck-blackbox/bootstrap.sh
+++ deployments/healthcheck-blackbox/bootstrap.sh
@@ -9,3 +9,10 @@ bootstrap_progress
 
 
 log_end_deployment
+
+echo -e "\033[2K
+▼ Healthcheck Blackbox
+  ▽ Prometheus:
+                    URL : http://localhost:3200
+               Username : admin
+               Password : abc123"
