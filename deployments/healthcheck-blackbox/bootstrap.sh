5d830aba50f1d34b80e129b361da44fd99739e37
David Garvey <davegarvey@hotmail.com>
2024-12-13 16:50:17 +0100

    minor amend

--- deployments/healthcheck-blackbox/bootstrap.sh
+++ deployments/healthcheck-blackbox/bootstrap.sh
@@ -5,13 +5,14 @@ source scripts/common.sh
 deployment="Health Check - Blackbox Exporter"
 
 log_start_deployment
-bootstrap_progress
 
+# TODO: ADD ANY NECESSARY BOOTSTRAPPING HERE
+bootstrap_progress
 
 log_end_deployment
 
 echo -e "\033[2K
-▼ Healthcheck Blackbox
+▼ Health Check - Blackbox Exporter
   ▽ Prometheus:
                     URL : http://localhost:3200
                Username : admin
