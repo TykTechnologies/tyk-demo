192d5c333c70e4e88cf27c040708b4520316275f
David Garvey <davegarvey@hotmail.com>
2025-01-03 17:24:56 +0100

    disable scripts

--- deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-1.sh
+++ deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-1.sh
@@ -1,8 +1,8 @@
-echo "This functionality is not implemented yet."
+# echo "This functionality is not implemented yet."
 
-# echo "Streams - Simple PubSub"
-# echo "Part 1: Using server-sent events"
-# echo "This script connects to a server-sent event endpoint, and waits to receive messages."
-# echo "Use script 3 to send messages."
+echo "Streams - Simple PubSub"
+echo "Part 1: Using server-sent events"
+echo "This script connects to a server-sent event endpoint, and waits to receive messages."
+echo "Use script 3 to send messages."
 
-# curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/sse
+curl http://tyk-gateway.localhost:8080/streams-simple-pubsub/sse
--- deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-2.sh
+++ deployments/tyk/scripts/examples/streams/simple-pubsub/simple-pubsub-2.sh
@@ -1,14 +1,14 @@
-echo "This functionality is not implemented yet."
+# echo "This functionality is not implemented yet."
 
-# #!/bin/bash
+#!/bin/bash
 
-# source scripts/common.sh
+source scripts/common.sh
 
-# check_for_wscat
+check_for_wscat
 
-# echo "Streams - Simple PubSub"
-# echo "Part 2: Using websockets"
-# echo "This script connects to a websocket endpoint, and waits to receive messages."
-# echo "Use script 3 to send messages."
+echo "Streams - Simple PubSub"
+echo "Part 2: Using websockets"
+echo "This script connects to a websocket endpoint, and waits to receive messages."
+echo "Use script 3 to send messages."
 
-# wscat -c ws://tyk-gateway.localhost:8080/streams-simple-pubsub/ws
+wscat -c ws://tyk-gateway.localhost:8080/streams-simple-pubsub/ws
