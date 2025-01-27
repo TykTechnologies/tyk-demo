fcae1c47833bb7f6f5e7d073d99e8356688ee0fa
David Garvey <davegarvey@hotmail.com>
2025-01-27 13:49:41 +0100

    grammar

--- deployments/tyk/scripts/examples/streams/authentication/post_data_unauth.sh
+++ deployments/tyk/scripts/examples/streams/authentication/post_data_unauth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Send POST request that will be rejected, due as not authenticated
+# Send POST request that will be rejected, as it's not authenticated
 curl -X POST http://tyk-gateway.localhost:8080/streams-authentication/post \
     -H "Content-Type: text/plain" \
     -d "This will fail" \
