eaf4e76d13d7e81661b5c8a05155aecaa7a909a6
Sedky <sedkyaboushamalah@gmail.com>
2025-03-21 08:24:54 -0400

    Added T&C

--- deployments/portal/bootstrap.sh
+++ deployments/portal/bootstrap.sh
@@ -25,19 +25,8 @@ set_docker_environment_value "ADMIN_ORG_ID" $dashboard_user_org_id
 set_docker_environment_value "TYK_DASHBOARD_API_ACCESS_CREDENTIALS" $dashboard_user_api_credentials
 
 # Postgres Env configuration for tyk portal
-set_docker_environment_value "PORTAL_HOST_PORT" 3001
-set_docker_environment_value "PORTAL_DATABASE_DIALECT" postgres
-set_docker_environment_value "POSTGRES_PASSWORD" secr3t
 set_docker_environment_value "PORTAL_DATABASE_CONNECTIONSTRING" "host=tyk-portal-postgres port=5432 dbname=portal user=admin password=$(grep "POSTGRES_PASSWORD=" .env | sed -E 's/^[A-Z_]+=(.+)$/\1/') sslmode=disable"
-set_docker_environment_value "PORTAL_DATABASE_ENABLELOGS" true
-set_docker_environment_value "PORTAL_THEMING_THEME" default
-set_docker_environment_value "PORTAL_THEMING_PATH" ./themes
 set_docker_environment_value "PORTAL_LICENSEKEY" $encoded_licence_jwt
-set_docker_environment_value "PORTAL_DOCRENDERER" stoplight
-set_docker_environment_value "PORTAL_REFRESHINTERVAL" 10
-set_docker_environment_value "PORTAL_LOG_LEVEL" debug
-set_docker_environment_value "PORTAL_LOG_FORMAT" dev
-set_docker_environment_value "PORTAL_AUDIT_LOG_ENABLE" true
 
 
 
