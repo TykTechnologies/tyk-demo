416f65710cdd951ea0446329402964a323a71411
David Garvey <davegarvey@hotmail.com>
2025-04-11 20:21:56 +0200

    update hostname

--- deployments/plugin-python-grpc/scripts/hmac.sh
+++ deployments/plugin-python-grpc/scripts/hmac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_URL=http://localhost:8080
+BASE_URL=http://tyk-gateway.localhost:8080
 ENDPOINT=/grpc-custom-auth/get
 HMAC_ALGORITHM=hmac-sha512
 HMAC_SECRET=c2VjcmV0
@@ -29,7 +29,7 @@ echo "signature: $signature"
 echo "url_encoded_signature: $url_encoded_signature"
 
 # Make the curl request using headers
-printf "\n\n----\n\nMaking request to  http://localhost:8080/grpc-custom-auth/get\n\n"
+printf "\n\n----\n\nMaking request to  $BASE_URL/grpc-custom-auth/get\n\n"
 
 response=$(curl -s -w "\n%{http_code}" -H "Date: ${date}" \
     -H "Authorization: Signature keyId=\"${KEY}\",algorithm=\"${HMAC_ALGORITHM}\",signature=\"${url_encoded_signature}\"" \
