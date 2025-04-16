d67f483c7823597c527369c016687dc0571b17d4
David Garvey <davegarvey@hotmail.com>
2025-04-16 11:11:48 +0200

    colours now defined in common script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,6 @@
 readonly BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 source "$BASE_DIR/scripts/test-common.sh"
 
-# Color and logging functions
-readonly RED='\033[0;31m'
-readonly GREEN='\033[0;32m'
-readonly BLUE='\033[0;34m'
-readonly NOCOLOUR='\033[0m'
-
 declare -a postman_results script_results statuses deployments
 
 log() {
