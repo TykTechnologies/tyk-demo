ef4bf9b4b4ceb09de4953aa7e9a6830414dced43
David Garvey <davegarvey@hotmail.com>
2025-05-08 16:46:23 +0200

    match

--- windows/Setup-Tyk-Demo.ps1
+++ windows/Setup-Tyk-Demo.ps1
@@ -60,7 +60,7 @@ try {
 
     # Check for Ubuntu
     Write-Status "Checking for Ubuntu distro"
-    $ubuntuInstalled = (wsl -l -v) -contains "Ubuntu"
+    $ubuntuInstalled = (wsl -l -v) -match "Ubuntu"
     if (-not $ubuntuInstalled) {
         Write-Status "Ubuntu is not installed. Please install it with 'wsl --install -d Ubuntu'" -Type "ERROR"
         exit 1
