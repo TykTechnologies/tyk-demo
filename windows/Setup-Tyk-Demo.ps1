d8c5618f16a8002be3c01eff1e8798edff79be18
David Garvey <davegarvey@hotmail.com>
2025-05-13 16:30:43 +0100

    Update Setup-Tyk-Demo.ps1

--- windows/Setup-Tyk-Demo.ps1
+++ windows/Setup-Tyk-Demo.ps1
@@ -12,34 +12,34 @@ function ValidatePrerequisites {
 
     # Check Docker
     if (Get-Command docker -ErrorAction SilentlyContinue) {
-        Write-Host "Docker is installed." -ForegroundColor Green
+        Write-Host "- Docker is installed." -ForegroundColor Green
     } else {
-        Write-Host "Docker is not installed." -ForegroundColor Red
+        Write-Host "- Docker is not installed." -ForegroundColor Red
         $status=$false
     }
 
     # Check Docker Compose 
     $composeAvailable = docker compose version 2>$null
     if ($LASTEXITCODE -eq 0) {
-        Write-Host "Docker Compose is installed." -ForegroundColor Green
+        Write-Host "- Docker Compose is installed." -ForegroundColor Green
     } else {
-        Write-Host "Docker Compose is not installed." -ForegroundColor Red
+        Write-Host "- Docker Compose is not installed." -ForegroundColor Red
         $status=$false
     }
 
     # Check WSL
     if (Get-Command wsl -ErrorAction SilentlyContinue) {
-        Write-Host "Windows Subsystem for Linux is installed." -ForegroundColor Green
+        Write-Host "- Windows Subsystem for Linux is installed." -ForegroundColor Green
     } else {
-        Write-Host "Windows Subsystem for Linux is not installed." -ForegroundColor Red
+        Write-Host "- Windows Subsystem for Linux is not installed." -ForegroundColor Red
         $status=$false
     }
 
     # Check if the Docker Desktop process is running
     if (Get-Process -Name "Docker Desktop" -ErrorAction SilentlyContinue) {
-        Write-Host "Docker Desktop is running." -ForegroundColor Green
+        Write-Host "- Docker Desktop is running." -ForegroundColor Green
     } else {
-        Write-Host "Docker Desktop is not installed." -ForegroundColor Red
+        Write-Host "- Docker Desktop is not installed." -ForegroundColor Red
         $status=$false
     }
 
@@ -52,9 +52,9 @@ function ValidateEnvironment {
     # Check for Tyk Demo distro
     $wslDistros = wsl.exe --list --quiet
     if ($wslDistros -contains $distroName) {
-        Write-Host "Tyk Demo WSL distro is present." -ForegroundColor Green
+        Write-Host "- Tyk Demo WSL distro is present." -ForegroundColor Green
     } else {
-        Write-Host "Tyk Demo WSL distro not is present." -ForegroundColor Yellow
+        Write-Host "- Tyk Demo WSL distro not is present." -ForegroundColor Yellow
         Write-Host "Creating Tyk Demo distro... "
         wsl --install ubuntu --name $distroName
         if ($LASTEXITCODE -eq 0) {
@@ -76,9 +76,9 @@ function ValidateEnvironment {
     $repoPath = "/opt/tyk-demo"
     wsl -d $distroName -e test -d $repoPath
     if ($LASTEXITCODE -eq 0) {
-        Write-Host "Tyk Demo repository is present." -ForegroundColor Green
+        Write-Host "- Tyk Demo repository is present." -ForegroundColor Green
     } else {
-        Write-Host "Tyk Demo repository is not present." -ForegroundColor Yellow
+        Write-Host "- Tyk Demo repository is not present." -ForegroundColor Yellow
         Write-Host "Cloning Tyk Demo repository... "
         wsl -d $distroName -e git clone https://github.com/TykTechnologies/tyk-demo $repoPath
         if ($LASTEXITCODE -eq 0) {
@@ -93,9 +93,9 @@ function ValidateEnvironment {
     $envFilePath = "$repoPath/.env"
     wsl -d $distroName -e bash -c "test -f '$envFilePath' && grep '^DASHBOARD_LICENCE=' '$envFilePath'"
     if ($LASTEXITCODE -eq 0) {
-        Write-Host "Tyk licence found." -ForegroundColor Green
+        Write-Host "- Tyk licence found." -ForegroundColor Green
     } else {
-        Write-Host "Tyk licence not found..." -ForegroundColor Yellow
+        Write-Host "- Tyk licence not found..." -ForegroundColor Yellow
         $newLicence = Read-Host "Paste your Tyk licence and press return"
         wsl -d $distroName -e bash -c "cd $repoPath && ./scripts/update-env.sh DASHBOARD_LICENCE $newLicence"
         if ($LASTEXITCODE -eq 0) {
@@ -104,6 +104,15 @@ function ValidateEnvironment {
             Write-Host "Error (exit code $($LASTEXITCODE))." -ForegroundColor Red
             return $false
         }
+    }
+
+    # Check for Docker in distro
+    $dockerResponse = wsl -d $distroName -e docker version 2>$null
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host "- Docker is available in Tyk Demo distro." -ForegroundColor Green
+    } else {
+        Write-Host "- Docker is not available in Tyk Demo distro" -ForegroundColor Red
+        Write-Host "To resolve, update Docker Desktop (Settings -> Resources -> WSL Integration) to enable WSL integration with $distroName distro. Apply changes and restart Docker Desktop."
     }
 
     return $true
@@ -117,16 +126,18 @@ if (-not (Test-AdminPrivileges)) {
     return $false
 }
 
-Write-Host "Validating Prerequisites" -ForegroundColor Cyan
+Write-Host "Validating Prerequisites:" -ForegroundColor Cyan
 
 if (-not (ValidatePrerequisites)) {
     Write-Host "Prerequisite check failed." -ForegroundColor Red
     exit 1
 }
 
-Write-Host "Validating Environment" -ForegroundColor Cyan
+Write-Host "Validating Environment:" -ForegroundColor Cyan
 
 if (-not (ValidateEnvironment)) {
     Write-Host "Environment check failed." -ForegroundColor Red
     exit 1
 }
+
+Write-Host "Tyk Demo environment is ready."
