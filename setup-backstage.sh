681d28a4b59c82053016638f4765a063940dc992
David Garvey <davegarvey@hotmail.com>
2025-05-20 17:01:52 +0200

    target service on host

--- setup-backstage.sh
+++ setup-backstage.sh
@@ -8,7 +8,7 @@ dashboard_api_key2=$(get_context_data "tyk2" "dashboard-user" "1" "api-key")
 dashboard_admin_api_credentials=$(cat deployments/tyk/volumes/tyk-dashboard/tyk_analytics.conf | jq -r .admin_secret 2>> logs/bootstrap.log)
 
 # Add API event webhook to default organisation
-updated_org_data=$(jq '.event_options += { "api_event": { "webhook": "http://localhost:7007/api/catalog/tyk/development/sync" } }' < deployments/tyk/data/tyk-dashboard/1/organisation.json)
+updated_org_data=$(jq '.event_options += { "api_event": { "webhook": "http://host.docker.internal:7007/api/catalog/tyk/development/sync" } }' < deployments/tyk/data/tyk-dashboard/1/organisation.json)
 api_response=$(curl $dashboard_base_url/admin/organisations/5e9d9544a1dcd60001d0ed20 --request PUT -s \
     -H "admin-auth: $dashboard_admin_api_credentials" \
     -d "$updated_org_data" 2>> logs/bootstrap.log)
