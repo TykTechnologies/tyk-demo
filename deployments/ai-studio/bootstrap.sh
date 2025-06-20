3fd64b0c8da08bcbf287570948c0bab5c9099040
David Garvey <davegarvey@hotmail.com>
2025-06-20 15:47:20 +0200

    prevent changes to ai studio database from being tracked

--- deployments/ai-studio/bootstrap.sh
+++ deployments/ai-studio/bootstrap.sh
@@ -7,11 +7,28 @@ deployment="AI Studio"
 log_start_deployment
 bootstrap_progress
 
-log_message "Generating docker compose command for ai-studio deployment..."
-$(generate_docker_compose_command) up -d --no-deps --force-recreate 1>/dev/null 2>>logs/bootstrap.log
-log_message "Done."
-log_end_deployment
+log_message "Removing AI Studio service"
+# this is to allow the database to be reset in the next step
+$(generate_docker_compose_command) rm -s -f -v tyk-ai-studio 1>>logs/bootstrap.log 2>&1
+log_ok
+bootstrap_progress
 
+log_message "Resetting AI Studio database"
+# this ensures that the AI Studio database is reset to a clean state on each bootstrap
+cp deployments/ai-studio/data/tyk-ai-studio/ai-studio.db deployments/ai-studio/volumes/tyk-ai-studio/db
+if [ $? -ne 0 ]; then
+    echo "ERROR: Failed to reset AI Studio database"
+    exit 1
+fi
+log_ok
+bootstrap_progress
+
+log_message "Recreate AI Studio service..."
+$(generate_docker_compose_command) up -d --no-deps 1>/dev/null 1>>logs/bootstrap.log 2>&1
+log_ok
+bootstrap_progress
+
+log_end_deployment
 
 echo -e "\033[2K 
 ▼ Tyk AI Studio
